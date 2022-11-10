7cc0e444aaeaeaf0d933c3fcd3adf7620b9e3111
Hannes <hannes.ryberg00+git@gmail.com>
2022-11-10 14:49:39 +0100

    Github actions working!!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
       <Grid.Col xs={8} lg={9}>
         <Group position="apart">
           <Title order={1}>
-            {gigs?.length === 0 ?? false ? 'Inga kommande spelningar :(' : 'Bajs i arslet'}
+            {gigs && gigs.length === 0 ? 'Inga kommande spelningar :(' : 'Kommande spelningar'}
           </Title>
         </Group>
         <Accordion>
