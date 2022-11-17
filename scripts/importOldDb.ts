881ec4945f706f3d38d31673b9958c0d1a3ec43e
CodeyBoi <hannes.ryberg00@gmail.com>
2022-11-17 09:17:11 +0100

    import script mostly working

--- scripts/importOldDb.ts
+++ scripts/importOldDb.ts
@@ -105,160 +105,167 @@ const parseDate = (date: string) => {
       first === "0" || first === "1" || first === "2" ? "20" : "19";
     return new Date(
       century +
-        date.slice(0, 2) +
-        "-" +
-        date.slice(2, 4) +
-        "-" +
-        date.slice(4, 6)
+      date.slice(0, 2) +
+      "-" +
+      date.slice(2, 4) +
+      "-" +
+      date.slice(4, 6)
     );
   }
   return new Date("1970-01-01");
 };
 
 const main = async () => {
-  // These objects are used to map the old database's IDs to the new ones
-  const gigIds: { [key: number]: number } = {};
-  const corpsIds: { [key: number]: number } = {};
-
   const prisma = new PrismaClient();
+  try {
+    await prisma.$transaction(async (tx) => {
+      // These objects are used to map the old database's IDs to the new ones
+      const gigIds: { [key: number]: number } = {};
+      const corpsIds: { [key: number]: number } = {};
 
-  if (process.argv.length < 3) {
-    console.log("Usage: node importOldDb.mjs <oldDbDump.json>");
-    process.exit(1);
-  }
+      if (process.argv.length < 3) {
+        console.log("Usage: node importOldDb.mjs <oldDbDump.json>");
+        process.exit(1);
+      }
 
-  const dirPath = process.argv[2];
-  if (!dirPath) {
-    console.log("Usage: node importOldDb.mjs <oldDbDump.json>");
-    process.exit(1);
-  }
+      const dirPath = process.argv[2];
+      if (!dirPath) {
+        console.log("Usage: node importOldDb.mjs <oldDbDump.json>");
+        process.exit(1);
+      }
 
-  console.log("Importing old db dump from directory " + dirPath + "...");
-  const filenames = fs.readdirSync(dirPath);
+      console.log("Importing old db dump from directory " + dirPath + "...");
+      const filenames = fs.readdirSync(dirPath);
 
-  const getTable = (table: string) => {
-    const filename = filenames.find((f) => f.endsWith(table + ".json"));
-    if (!filename) {
-      console.log("Could not find file for table " + table);
-      process.exit(1);
-    }
-    let data = fs.readFileSync(path.join(dirPath, filename)).toString();
-    while (data.toString().charAt(0) !== "{") {
-      data = data.slice(1);
-    }
-    return JSON.parse(data);
-  };
+      const getTable = (table: string) => {
+        const filename = filenames.find((f) => f.endsWith(table + ".json"));
+        if (!filename) {
+          console.log("Could not find file for table " + table);
+          process.exit(1);
+        }
+        let data = fs.readFileSync(path.join(dirPath, filename)).toString();
+        while (data.toString().charAt(0) !== "{") {
+          data = data.slice(1);
+        }
+        return JSON.parse(data);
+      };
 
-  console.log("Importing events...");
-  const eventTable = getTable("Events");
-  for (const row of eventTable.data) {
-    const {
-      Id,
-      Name,
-      Description,
-      EventDate,
-      Times,
-      Other,
-      Other2,
-      EventTypeID,
-      Adress,
-    } = row;
+      console.log("Importing events...");
+      const eventTable = getTable("Events");
+      for (const row of eventTable.data) {
+        const {
+          Id,
+          Name,
+          Description,
+          EventDate,
+          Times,
+          Other,
+          Other2,
+          EventTypeID,
+          Adress,
+        } = row;
 
-    const date = parseDate(EventDate);
-    if (date.getFullYear() >= 2023) {
-      continue;
-    }
+        const date = parseDate(EventDate);
+        if (date.getFullYear() >= 2023) {
+          continue;
+        }
 
-    const timeRegex = /\d{1,2}[\.:]\d{2}/g;
-    const [meetup = undefined, start = undefined] = [
-      ...Times.matchAll(timeRegex),
-    ].map((m) => m[0]);
+        const timeRegex = /\d{1,2}[\.:]\d{2}/g;
+        const [meetup = undefined, start = undefined] = [
+          ...Times.matchAll(timeRegex),
+        ].map((m) => m[0]);
 
-    const gig = {
-      title: Name,
-      description: Description,
-      date: date.toString() === "Invalid Date" ? new Date(1970, 0, 1) : date,
-      meetup,
-      start,
-      checkbox1: Other,
-      checkbox2: Other2,
-      location: Adress,
-      points: 1,
-      typeId: gigTypeIds[EventTypeID] || 1,
-    };
+        const gig = {
+          title: Name,
+          description: Description,
+          date: date.toString() === "Invalid Date" ? new Date(1970, 0, 1) : date,
+          meetup,
+          start,
+          checkbox1: Other,
+          checkbox2: Other2,
+          location: Adress,
+          points: 1,
+          typeId: gigTypeIds[EventTypeID] || 1,
+        };
 
-    const id = parseInt(Id);
-    gigIds[id] = (await prisma.gig.create({ data: gig })).id;
-  }
+        const id = parseInt(Id);
+        gigIds[id] = (await tx.gig.create({ data: gig })).id;
+      }
 
-  console.log("Importing users...");
-  const userTable = getTable("Users");
-  for (const row of userTable.data) {
-    const {
-      UserID,
-      Number,
-      FirstName,
-      LastName,
-      Email,
-      BNumber,
-      Section,
-      AltSec,
-      // Stamledare,
-      Status,
-    } = row;
+      console.log("Importing users...");
+      const userTable = getTable("Users");
+      for (const row of userTable.data) {
+        const {
+          UserID,
+          Number,
+          FirstName,
+          LastName,
+          Email,
+          BNumber,
+          Section,
+          AltSec,
+          // Stamledare,
+          Status,
+        } = row;
 
-    if (!Email) {
-      continue;
-    }
+        if (!Email) {
+          continue;
+        }
 
-    const user = {
-      email: Email,
-    };
-    const userId = (
-      await prisma.user.upsert({
-        where: { email: Email },
-        update: user,
-        create: user,
-      })
-    ).id;
-    console.log(`Importing user ${FirstName} ${LastName} with email ${Email}`);
-    const instrumentIndex = parseInt(Section);
-    const altInstrumentIndex = parseInt(AltSec);
-    const instruments = {
-      create: [
-        {
-          instrument: {
+        const user = {
+          email: Email,
+        };
+        const userId = (
+          await tx.user.upsert({
+            where: { email: Email },
+            update: user,
+            create: user,
+          })
+        ).id;
+        console.log(`Importing user ${FirstName} ${LastName} with email ${Email}`);
+        const instrumentIndex = parseInt(Section);
+        const altInstrumentIndex = parseInt(AltSec);
+        const instruments = {
+          create: [
+            {
+              instrument: {
+                connect: {
+                  name: OLD_INSTRUMENTS[instrumentIndex - 1],
+                },
+              },
+              isMainInstrument: true,
+            },
+            {
+              instrument: {
+                connect: {
+                  name: OLD_INSTRUMENTS[altInstrumentIndex - 1],
+                },
+              },
+              isMainInstrument: false,
+            },
+          ].filter((i) => i.instrument.connect.name !== undefined),
+        };
+
+        const corps = {
+          number: Number ? parseInt(Number) : undefined,
+          firstName: FirstName,
+          lastName: LastName,
+          bNumber: BNumber ? parseInt(BNumber.slice(1)) : undefined,
+          instruments,
+          isActive: Status === 1,
+          user: {
             connect: {
-              name: OLD_INSTRUMENTS[instrumentIndex - 1],
+              id: userId,
             },
           },
-          isMainInstrument: true,
-        },
-        {
-          instrument: {
-            connect: {
-              name: OLD_INSTRUMENTS[altInstrumentIndex - 1],
-            },
-          },
-          isMainInstrument: false,
-        },
-      ].filter((i) => i.instrument.connect.name !== undefined),
-    };
-
-    const corps = {
-      number: Number ? parseInt(Number) : undefined,
-      firstName: FirstName,
-      lastName: LastName,
-      bNumber: BNumber ? parseInt(BNumber.slice(1)) : undefined,
-      instruments,
-      isActive: Status === 1,
-      user: {
-        connect: {
-          id: userId,
-        },
-      },
-    };
-    corpsIds[UserID] = (await prisma.corps.create({ data: corps })).id;
+        };
+        corpsIds[UserID] = (await tx.corps.create({ data: corps })).id;
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
