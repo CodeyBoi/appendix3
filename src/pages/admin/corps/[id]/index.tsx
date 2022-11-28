c6e2abedb1d85e396a19bf37b859dc0762f9c0be
CodeyBoi <hannes.ryberg00@gmail.com>
2022-11-28 10:44:56 +0100

    updating user form

--- src/pages/admin/corps/[id]/index.tsx
+++ src/pages/admin/corps/[id]/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   TextInput,
   Button,
   Group,
   Box,
   Select,
-  PasswordInput,
   SimpleGrid,
   Title,
+  LoadingOverlay,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useRouter } from "next/router";
@@ -32,7 +32,7 @@ const CreateCorps = () => {
   const utils = trpc.useContext();
 
   const [loading, setLoading] = React.useState(!creatingCorps);
-  const [submittning, setSubmitting] = React.useState(false);
+  const [submitting, setSubmitting] = React.useState(false);
 
   const { data: instruments } = trpc.instrument.getAll.useQuery();
   const { data: corps } = trpc.corps.getSelf.useQuery(undefined, {
@@ -50,10 +50,33 @@ const CreateCorps = () => {
         /^[0-9]+/.test(value)
           ? null
           : "Nummer får bara innehålla siffror (duh)",
+      bNumber: (value) =>
+        /^[0-9]+/.test(value)
+          ? null
+          : "Balettnummer får bara innehålla siffror (duh)",
       email: (value) =>
         /^\S+@\S+$/.test(value) ? null : "Ogiltig emailadress",
     },
   });
+
+  useEffect(() => {
+    if (!corps) {
+      return;
+    }
+    const mainInstrument = corps.instruments.find((i) => i.isMainInstrument)?.instrument.name;
+    const otherInstruments = corps.instruments.filter((i) => !i.isMainInstrument).map((i) => i.instrument.name);
+    form.setValues({
+      firstName: corps.firstName,
+      lastName: corps.lastName,
+      number: corps.number?.toString() || "",
+      bNumber: corps.bNumber?.toString() || "",
+      email: corps.user.email ?? "",
+      mainInstrument,
+      otherInstruments,
+    });
+    setLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [corps]);
 
   const handleSubmit = async (values: FormValues) => {
     setSubmitting(true);
@@ -65,68 +88,51 @@ const CreateCorps = () => {
   return (
     <Box sx={{ maxWidth: 700, fontFamily: "Castellar" }} mx="auto">
       <Title order={2}>Skapa användare</Title>
-      <form
-        onSubmit={form.onSubmit(handleSubmit)}
-      >
-        <SimpleGrid
-          cols={2}
-          spacing="lg"
-          breakpoints={[{ maxWidth: 600, cols: 1 }]}
-        >
-          <TextInput
-            withAsterisk
-            label="Förnamn"
-            placeholder="Förnamn"
-            {...form.getInputProps("firstName")}
-          />
-          <TextInput
-            withAsterisk
-            label="Efternamn"
-            placeholder="Efternamn"
-            {...form.getInputProps("lastName")}
-          />
-          <TextInput
-            label="Nummer"
-            placeholder="Nummer (lämnas tomt om du inte har något)"
-            {...form.getInputProps("number")}
-          />
-          <Select
-            withAsterisk
-            label="Huvudinstrument"
-            placeholder="Välj instrument..."
-            searchable
-            nothingFound="Inget hittades"
-            data={INSTRUMENTS}
-            {...form.getInputProps("mainInstrument")}
-          />
-          <TextInput
-            withAsterisk
-            label="Användarnamn"
-            placeholder="Användarnamn"
-            {...form.getInputProps("username")}
-          />
-          <TextInput
-            withAsterisk
-            label="Email"
-            placeholder="din@email.com"
-            {...form.getInputProps("email")}
-          />
-          <PasswordInput
-            withAsterisk
-            label="Lösenord"
-            placeholder="Lösenord"
-            {...form.getInputProps("password")}
-          />
-          <PasswordInput
-            withAsterisk
-            label="Bekräfta lösenord"
-            placeholder="Bekräfta lösenord"
-            {...form.getInputProps("passwordConfirm")}
-          />
-        </SimpleGrid>
-        <Group position="right" mt="md">
-          <Button type="submit">Registrera</Button>
-        </Group>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <div>
+          <LoadingOverlay visible={loading || submitting} />
+          <SimpleGrid
+            cols={2}
+            spacing="lg"
+            breakpoints={[{ maxWidth: 600, cols: 1 }]}
+          >
+            <TextInput
+              withAsterisk
+              label="Förnamn"
+              placeholder="Förnamn"
+              {...form.getInputProps("firstName")}
+            />
+            <TextInput
+              withAsterisk
+              label="Efternamn"
+              placeholder="Efternamn"
+              {...form.getInputProps("lastName")}
+            />
+            <TextInput
+              label="Nummer"
+              placeholder="Nummer (lämnas tomt om du inte har något)"
+              {...form.getInputProps("number")}
+            />
+            <Select
+              withAsterisk
+              label="Huvudinstrument"
+              placeholder="Välj instrument..."
+              searchable
+              nothingFound="Inget instrument hittades"
+              data={instruments}
+              {...form.getInputProps("mainInstrument")}
+            />
+            <TextInput
+              withAsterisk
+              label="Email"
+              placeholder="din@email.com"
+              {...form.getInputProps("email")}
+            />
+          </SimpleGrid>
+          <Group position="right" mt="md">
+            <Button type="submit">Registrera</Button>
+          </Group>
+        </div>
       </form>
     </Box>
   );
