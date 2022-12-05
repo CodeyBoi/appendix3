1aeba32c8e2849a24ec5304a593c58b168ba00f0
Hannes <hannes.ryberg00+git@gmail.com>
2022-12-05 01:32:30 +0100

    adding navbar

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -2,12 +2,14 @@ import { AppShell } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import { ReactElement } from "react";
 import AppendixHeader from "./header";
+import AppendixNavbar from "./navbar";
 
 const AppContainer = ({ children }: { children: ReactElement }) => {
   const session = useSession();
   return (
     <AppShell
       header={<AppendixHeader />}
+      navbar={<AppendixNavbar />}
       hidden={!session || session.status !== 'authenticated'}
       padding={24}
     >
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Header, Group, Button } from '@mantine/core';
-import Logo from './logo';
-import AdminMenu from './admin-menu';
-import { IconClipboard, IconLogout, IconUser, IconSpeakerphone } from '@tabler/icons';
-import { signOut, useSession } from 'next-auth/react';
-import { NextLink } from '@mantine/next';
-
-const getOperatingYear = () => {
-  const date = new Date();
-  const year = date.getFullYear();
-  // If month is September or later, return current year, else return previous year
-  return date.getMonth() >= 8 ? year : year - 1;
-}
+import React from "react";
+import { Header, Group, Button } from "@mantine/core";
+import Logo from "./logo";
+import AdminMenu from "./admin-menu";
+import { IconLogout, IconUser } from "@tabler/icons";
+import { signOut, useSession } from "next-auth/react";
+import { NextLink } from "@mantine/next";
 
 const AppendixHeader = () => {
-
   const { data: session } = useSession();
-  const isAdmin = session?.user?.corps?.role?.name === 'admin';
+  const isAdmin = session?.user?.corps?.role?.name === "admin";
 
   return (
-    <Header height={60} p="sm" sx={(theme) => ({
-      backgroundColor: theme?.colors?.red?.[5],
-      color: theme.white,
-      zIndex: 516,
-    })}>
+    <Header
+      height={60}
+      p="sm"
+      sx={(theme) => ({
+        backgroundColor: theme?.colors?.red?.[5],
+        color: theme.white,
+        zIndex: 516,
+      })}
+    >
       <Group position="apart">
         <Logo />
         <Group spacing={0}>
           {isAdmin && <AdminMenu />}
-          <Button px={6} leftIcon={<IconSpeakerphone />} component={NextLink} href='/gig'>Spelningar</Button>
-          <Button px={6} leftIcon={<IconClipboard />} size="sm" component={NextLink} href={`/stats/${getOperatingYear()}`}>Statistik</Button>
-          <Button px={6} leftIcon={<IconUser />} size='sm' component={NextLink} href="/account">Mina sidor</Button>
-          <Button px={6} leftIcon={<IconLogout />} size='sm' onClick={() => signOut()}>Logga ut</Button>
+          <Button
+            px={6}
+            leftIcon={<IconLogout />}
+            size="sm"
+            onClick={() => signOut()}
+          >
+            Logga ut
+          </Button>
         </Group>
       </Group>
     </Header>
   );
-}
+};
 
 export default AppendixHeader;
