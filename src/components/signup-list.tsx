fdd229ab2a3aea9784d2e2bc40f1b11924a82435
Hannes Ryberg <47188830+CodeyBoi@users.noreply.github.com>
2022-12-07 14:07:29 +0100

    Some UI fixes to signup page (#36)
    * Removed corps from selection if they are signed up
    * Adding loading message to stats
    * Refactored sort function for signups

--- src/components/signup-list.tsx
+++ src/components/signup-list.tsx
@@ -1,10 +1,22 @@
-import React, { useMemo } from "react";
-import { Box, Center, Table, Checkbox, CloseButton, Tooltip, Title, Button, Group, Space } from "@mantine/core";
+import React, { useMemo, useState } from "react";
+import {
+  Box,
+  Center,
+  Table,
+  Checkbox,
+  CloseButton,
+  Tooltip,
+  Title,
+  Button,
+  Group,
+  Space,
+} from "@mantine/core";
 import { trpc } from "../utils/trpc";
 import { useForm } from "@mantine/form";
 import { IconUser } from "@tabler/icons";
 import { useQueryClient } from "@tanstack/react-query";
 import MultiSelectCorpsii from "./multi-select-corpsii";
+import Loading from "./loading";
 
 interface SignupListProps {
   gigId: string;
@@ -32,61 +44,80 @@ interface SignupListProps {
 // };
 
 const SignupList = ({ gigId }: SignupListProps) => {
-  
   const queryClient = useQueryClient();
   const utils = trpc.useContext();
 
-  const { data: signups } =
-    trpc.gig.getSignups.useQuery({ gigId }, { enabled: !!gigId });
+  const { data: signups, isInitialLoading: signupsLoading } = trpc.gig.getSignups.useQuery(
+    { gigId },
+    { enabled: !!gigId }
+  );
 
   const { data: corps } = trpc.corps.getSelf.useQuery();
   const isAdmin = corps?.role?.name === "admin";
 
   const { data: instruments } = trpc.instrument.getAll.useQuery();
   // An object which maps instrument names to their position in the INSTRUMENTS array
-  const instrumentPrecedence: { [key: string]: number } = useMemo(() => instruments?.reduce((acc, instrument) => {
-    (acc as { [key: string]: number })[instrument.name] = instrument.id;
-    return acc;
-  }, {}) ?? [], [instruments]);
+  const instrumentPrecedence: { [key: string]: number } = useMemo(
+    () =>
+      instruments?.reduce((acc, instrument) => {
+        (acc as { [key: string]: number })[instrument.name] = instrument.id;
+        return acc;
+      }, {}) ?? [],
+    [instruments]
+  );
 
   // Sorts the list of corpsii by instrument precedence, then number, then last name, then first name.
-  const signupsSorted = useMemo(() => signups?.sort((a, b) => {
-    if (a.instrument === b.instrument) {
-      if (!a.number && !b.number) {
-        if (a.lastName === b.lastName) {
-          return a.firstName.localeCompare(b.firstName);
-        } else {
+  const signupsSorted = useMemo(
+    () =>
+      signups?.sort((a, b) => {
+        // Compare instrument precedence
+        if (a.instrument !== b.instrument) {
+          const aPrio = instrumentPrecedence[a.instrument] ?? Infinity;
+          const bPrio = instrumentPrecedence[b.instrument] ?? Infinity;
+          return aPrio - bPrio;
+        }
+
+        // Compare numbers
+        if (a.number || b.number) {
+          return (a.number || Infinity) - (b.number || Infinity);
+        }
+
+        // Compare last name
+        if (a.lastName !== b.lastName) {
           return a.lastName.localeCompare(b.lastName);
         }
-      } else {
-        return (a.number ?? Infinity) - (b.number ?? Infinity);
-      }
-    } else {
-      if (!instrumentPrecedence) {
-        return 0;
-      }
-      return (instrumentPrecedence[a.instrument] ?? Infinity) - (instrumentPrecedence[b.instrument] ?? Infinity);
-    }
-  }) ?? [], [signups, instrumentPrecedence]);
+
+        // Compare first name
+        return a.firstName.localeCompare(b.firstName);
+      }) ?? [],
+    [signups, instrumentPrecedence]
+  );
 
   // Divide the list of corpsii into people who answered yes and people who answered maybe
-  const splitList = signupsSorted?.reduce((acc, signup) => {
-    if (signup.signupStatus === 'Ja') {
-      acc.yesList.push(signup);
-    } else if (signup.signupStatus === 'Kanske') {
-      acc.maybeList.push(signup);
+  const splitList = signupsSorted?.reduce(
+    (acc, signup) => {
+      if (signup.signupStatus === "Ja") {
+        acc.yesList.push(signup);
+      } else if (signup.signupStatus === "Kanske") {
+        acc.maybeList.push(signup);
+      }
+      return acc;
+    },
+    {
+      yesList: [] as typeof signupsSorted,
+      maybeList: [] as typeof signupsSorted,
     }
-    return acc;
-  }, { yesList: [] as typeof signupsSorted, maybeList: [] as typeof signupsSorted });
+  );
 
-  const yesList = splitList?.yesList ?? [];
-  const maybeList = splitList?.maybeList ?? [];
+  const yesList = splitList?.yesList;
+  const maybeList = splitList?.maybeList;
 
   const form = useForm({
     initialValues: { corpsIds: [] as string[] },
     validate: {
-      corpsIds: (value) => value.length > 0 ? null : 'Du måste välja minst ett corps',
-    }
+      corpsIds: (value) =>
+        value.length > 0 ? null : "Du måste välja minst ett corps",
+    },
   });
 
   const addSignups = trpc.gig.addSignups.useMutation({
@@ -98,6 +129,8 @@ const SignupList = ({ gigId }: SignupListProps) => {
     },
   });
 
+  const editAttendance = trpc.gig.editAttendance.useMutation();
+
   const removeSignup = trpc.gig.removeSignup.useMutation({
     onSuccess: async () => {
       await utils.gig.getSignup.invalidate();
@@ -105,15 +138,13 @@ const SignupList = ({ gigId }: SignupListProps) => {
     },
   });
 
-  const editAttendance = trpc.gig.editAttendance.useMutation();
-
   const handleDelete = (corpsId: string) => {
-    if (window.confirm('Är du säker på att du vill ta bort anmälningen?')) {
+    if (window.confirm("Är du säker på att du vill ta bort anmälningen?")) {
       removeSignup.mutateAsync({ corpsId, gigId });
     }
   };
 
-  /** Code from here on is absolutely horrible, but it works. 
+  /** Code from here on is absolutely horrible, but it works.
    *  Travelers beware.
    */
 
@@ -121,27 +152,35 @@ const SignupList = ({ gigId }: SignupListProps) => {
     if (signups.length === 0) {
       return;
     }
-
     return (
       <tbody>
         {signups.map((signup) => {
           return (
             <tr key={signup.corpsId}>
-              <td style={{ paddingTop: 0, paddingBottom: 0 }}>{signup.instrument}</td>
-              <td style={{ paddingTop: 0, paddingBottom: 0 }}>{signup.number ?? 'p.e.'}</td>
-              <td style={{ paddingTop: 0, paddingBottom: 0 }}>{signup.firstName + ' ' + signup.lastName}</td>
-              {isAdmin &&
+              <td style={{ paddingTop: 0, paddingBottom: 0 }}>
+                {signup.instrument}
+              </td>
+              <td style={{ paddingTop: 0, paddingBottom: 0 }}>
+                {signup.number ?? "p.e."}
+              </td>
+              <td style={{ paddingTop: 0, paddingBottom: 0 }}>
+                {signup.firstName + " " + signup.lastName}
+              </td>
+              {isAdmin && (
                 <>
                   <td style={{ paddingTop: 0, paddingBottom: 0 }}>
                     <Center>
                       <Checkbox
                         styles={{ root: { display: "flex" } }}
                         defaultChecked={signup.attended}
-                        onChange={(event) => editAttendance.mutateAsync({
-                          gigId,
-                          corpsId: signup.corpsId,
-                          attended: event.target.checked
-                        })} />
+                        onChange={(event) =>
+                          editAttendance.mutateAsync({
+                            gigId,
+                            corpsId: signup.corpsId,
+                            attended: event.target.checked,
+                          })
+                        }
+                      />
                     </Center>
                   </td>
                   <td style={{ paddingTop: 0, paddingBottom: 0 }}>
@@ -155,57 +194,79 @@ const SignupList = ({ gigId }: SignupListProps) => {
                     </Center>
                   </td>
                 </>
-              }
+              )}
             </tr>
           );
         })}
       </tbody>
     );
-  }
+  };
 
   const yesTable = signupsToTable(yesList);
   const maybeTable = signupsToTable(maybeList);
-  
+
   return (
     <Box>
-      {isAdmin &&
-        <form onSubmit={form.onSubmit((values) => addSignups.mutateAsync({ corpsIds: values.corpsIds, gigId, status: 'Ja' }))}>
+      {isAdmin && (
+        <form
+          onSubmit={form.onSubmit((values) =>
+            addSignups.mutateAsync({
+              corpsIds: values.corpsIds,
+              gigId,
+              status: "Ja",
+            })
+          )}
+        >
           <Space h="sm" />
-          <Group position='apart'>
+          <Group position="apart" noWrap>
             <MultiSelectCorpsii
-              placeholder='Lägg till anmälningar...'
+              placeholder="Lägg till anmälningar..."
               limit={30}
               maxDropdownHeight={350}
               icon={<IconUser />}
-              {...form.getInputProps('corpsIds')}
+              filter={(_value, selected, item) =>
+                !selected &&
+                (!signups || !signups.some((s) => s.corpsId === item.value))
+              }
+              {...form.getInputProps("corpsIds")}
             />
             <Button type="submit">Lägg till anmälan</Button>
           </Group>
         </form>
-      }
+      )}
       <Space h="sm" />
-      {yesList && (
+      {signupsLoading && <Loading msg="Laddar anmälningar..." />}
+      {!signupsLoading && (
         <>
-          {yesList.length > 0 && <Title order={3}>Dessa är anmälda:</Title>}
-          {yesList.length == 0 && <Title order={3}><i>Ingen är anmäld än. Kanske kan du bli den första?</i></Title>}
+          {yesList.length === 0 ? (
+            <Title order={3}>
+              <i>Ingen är anmäld än. Kanske kan du bli den första?</i>
+            </Title>
+          ) : (
+            <>
+              <Title order={3}>Dessa är anmälda:</Title>
+              <Table sx={{ tableLayout: "fixed" }}>
+                <thead>
+                  <tr>
+                    <th>Instrument</th>
+                    <th>Nummer</th>
+                    <th>Namn</th>
+                    {isAdmin && (
+                      <th>
+                        <Center>Närvaro</Center>
+                      </th>
+                    )}
+                    {isAdmin && <th></th>}
+                  </tr>
+                </thead>
+                {yesTable}
+              </Table>
+            </>
+          )}
         </>
       )}
-      {yesList.length > 0 &&(
-      <Table sx={{ tableLayout: "fixed" }}>
-        <thead>
-          <tr>
-            <th>Instrument</th>
-            <th>Nummer</th>
-            <th>Namn</th>
-            {isAdmin && <th><Center>Närvaro</Center></th>}
-            {isAdmin && <th></th>}
-          </tr>
-        </thead>
-        {yesTable}
-      </Table>
-      )}
       <br />
-      {maybeList.length > 0 && (
+      {maybeList && maybeList.length > 0 && (
         <>
           <Title order={3}>Dessa kanske kommer:</Title>
           <Table sx={{ tableLayout: "fixed" }}>
@@ -223,6 +284,6 @@ const SignupList = ({ gigId }: SignupListProps) => {
       )}
     </Box>
   );
-}
+};
 
 export default SignupList;
