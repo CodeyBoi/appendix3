d5532aebd00cce4ea9617efa1abde1f58b9a6b13
Hannes <hannes.ryberg00+git@gmail.com>
2022-12-08 13:53:38 +0100

    Updated login email

--- src/utils/email-auth.ts
+++ src/utils/email-auth.ts
@@ -25,7 +25,7 @@ const sendVerificationRequest = async (params: SendVerificationRequestParams) =>
   const result = await transport.sendMail({
     to: identifier,
     from: provider.from,
-    subject: `Poo poo head :) to ${host}`,
+    subject: `Inloggningslänk till Blindtarmen`,
     text: text({ url, host }),
     html: html({ url, host, theme }),
   })
@@ -44,51 +44,59 @@ const sendVerificationRequest = async (params: SendVerificationRequestParams) =>
  * @note We don't add the email address to avoid needing to escape it, if you do, remember to sanitize it!
  */
  function html(params: { url: string; host: string; theme: Theme }) {
-  const { url, host, theme } = params
-
-  const escapedHost = host.replace(/\./g, "&#8203;.")
-
-  const brandColor = theme.brandColor || "#346df1"
-  const color = {
-    background: "#f9f9f9",
-    text: "#444",
-    mainBackground: "#fff",
-    buttonBackground: brandColor,
-    buttonBorder: brandColor,
-    buttonText: theme.buttonText || "#fff",
-  }
-
+  const { url } = params
   return `
-<body style="background: ${color.background};">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        Sign in to <strong>${escapedHost}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`
+    <body style="background: linear-gradient(215deg, #810300 0%, #210000 100%); border-radius: 32px; min-height: 400px; display: flex; align-items: center;">
+      <table
+        width="100%"
+        border="0"
+        cellspacing="20"
+        cellpadding="0"
+        style="max-width: 600px; margin: auto; border-radius: 10px; font-family: Helvetica, sans-serif;"
+      >
+        <tr>
+          <td
+            align="center"
+            style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, sans-serif; color: white"
+          >
+            Välkommen till <strong style="color: #ce0c00;">Blindtarmen</strong>!
+          </td>
+        </tr>
+        <tr>
+          <td
+            align="center"
+            style="padding: 10px 0px; font-size: 16px; font-family: Helvetica, sans-serif; color: white"
+          >
+            Klicka på knappen nedan för att slutföra inloggningsprocessen.
+          </td>
+        </tr>
+        <td align="center" style="padding: 20px 0">
+          <table border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td
+                align="center"
+                style="
+                  border-radius: 5px;
+                  background-image: linear-gradient(
+                    185deg,
+                    #ce0c00 0%,
+                    darkred 100%
+                  );
+                "
+              >
+                <a
+                  href="${url}"
+                  target="_blank"
+                  style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: #FFFFFF; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 0; display: inline-block; font-weight: bold;"
+                  >Logga in</a
+                >
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </body>`
 }
 
 /** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
