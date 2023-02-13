2498562acbec433c174cce44ac18d1ca37575672
carlhampuswall <carl.hampus.wall@gmail.com>
2023-02-13 22:30:14 +0100

    Almost working fick for theme render on first fetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import type { Session } from 'next-auth';
-import type { AppType } from 'next/app';
+import type { AppContext, AppProps } from 'next/app';
 import { trpc } from '../utils/trpc';
 import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { useHotkeys } from '@mantine/hooks';
@@ -13,12 +13,21 @@ import useColorScheme from '../hooks/use-color-scheme';
 import 'dayjs/locale/sv';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import App from 'next/app';
+import cookieParser from '../utils/cookie-parser';
 
-const MyApp: AppType<{ session: Session | null }> = ({
+interface CustomAppProps {
+  session: Session | null;
+  colorScheme: string;
+}
+
+const MyApp = ({
   Component,
-  pageProps: { session, ...pageProps },
-}) => {
+  pageProps: { session, colorScheme: fetchedColorScheme, ...pageProps },
+}: AppProps<CustomAppProps>) => {
   const [mounted, setMounted] = useState(false);
+
+  console.log(fetchedColorScheme);
 
   // Allows user to toggle between light and dark mode by pressing `mod+Y`
   const { colorScheme, toggleColorScheme } = useColorScheme();
@@ -52,5 +61,32 @@ const MyApp: AppType<{ session: Session | null }> = ({
     </ColorSchemeProvider>
   );
 };
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const { ctx } = context;
+  const appProps = await App.getInitialProps(context);
+  const cookie = ctx.req?.headers.cookie;
+  if (cookie) {
+    const isProd = process.env.NODE_ENV !== 'development';
+    const cookies = cookieParser(cookie);
+    const sessionToken =
+      cookies[
+        isProd ? '__Secure-next-auth.session-token' : 'next-auth.session-token'
+      ];
+    const colorScheme = cookies['mantine-color-scheme'];
+    console.log(colorScheme);
+    console.log(prisma?.user.findUnique({ where: { id: '1' } }));
+
+    appProps.pageProps['colorScheme'] = colorScheme;
+  }
+
+  return { ...appProps };
+};
+
+// MyApp.getInitialProps = async (ctx: AppContext) => {
+//   //const cookies = req?.cookies;
+//   console.log(ctx);
+//   return {};
+// };
 
 export default trpc.withTRPC(MyApp);
---
+++ src/utils/cookie-parser.ts
@@ -0,0 +1,26 @@
+interface ObjectType {
+  [key: string]: string;
+}
+
+const cookieParser = (cookieString: string) => {
+  if (cookieString === '') return {};
+
+  let pairs = cookieString.split(';');
+
+  let splittedPairs = pairs.map((cookie) => cookie.split('='));
+
+  const cookieObj = splittedPairs.reduce(
+    (obj: ObjectType, cookie: string[]) => {
+      obj[decodeURIComponent(cookie[0]!.trim())] = decodeURIComponent(
+        cookie[1]!.trim(),
+      );
+
+      return obj;
+    },
+    {},
+  );
+
+  return cookieObj;
+};
+
+export default cookieParser;
