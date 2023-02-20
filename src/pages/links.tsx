e43377000ebf9b87d4e0f3abd3b4e2c9d9d4fc33
CodeyBoi <hannes.ryberg00@gmail.com>
2023-02-20 23:19:22 +0100

    Added links to link page

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Text, Title } from '@mantine/core';
-
-const style = {
-  color: 'blue',
-  textDecoration: 'underline',
-  cursor: 'pointer',
-};
+import { Text, Title, useMantineTheme } from '@mantine/core';
 
 const Links = () => {
+  const theme = useMantineTheme();
+  const style = {
+    color: `${theme.colors[theme.primaryColor ?? 'red']?.[5]}`,
+    textDecoration: 'underline',
+    cursor: 'pointer',
+  };
   return (
     <div>
-      <Title order={2}>Länkar</Title>
-      <Text mt={12}>
+      <Title order={1}>Länkar</Title>
+      <Text size='lg' mt={12}>
         <ul>
           <li>
             <a style={style} href='https://www.bleckhornen.org/'>
@@ -29,6 +29,16 @@ const Links = () => {
               href='https://www.facebook.com/groups/342920649132131'
             >
               Facebook-Tarmen
+            </a>
+          </li>
+          <li>
+            <a style={style} href='https://discord.gg/2wZafpfqR3'>
+              Bleckhornens discordserver
+            </a>
+          </li>
+          <li>
+            <a style={style} href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+              Nakenbilder av Arvid Tarmén
             </a>
           </li>
         </ul>
