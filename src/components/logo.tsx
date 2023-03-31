0ec9be1af66d7b3493d1333c4da67900832693d4
CodeyBoi <hannes.ryberg00@gmail.com>
2023-03-31 13:39:07 +0200

    On 1st of April change logo to Bihålan

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,30 @@
-import React from "react";
-import { Text } from "@mantine/core";
-import { NextLink } from "@mantine/next";
+import React from 'react';
+import { Text } from '@mantine/core';
+import { NextLink } from '@mantine/next';
 
 const Logo = () => {
+  const date = new Date();
+  const isAprilFools = date.getMonth() === 3 && date.getDate() === 1;
   return (
+    // <Text
+    //   sx={{
+    //     fontSize: "1.5rem",
+    //     fontFamily: "Castellar",
+    //   }}
+    //   component={NextLink}
+    //   href="/"
+    // >Blindtarmen</Text>
     <Text
       sx={{
-        fontSize: "1.5rem",
-        fontFamily: "Castellar",
+        fontSize: '1.5rem',
+        fontFamily: 'Castellar',
       }}
       component={NextLink}
-      href="/"
-    >Blindtarmen</Text>
+      href='/'
+    >
+      {isAprilFools ? 'Bihålan' : 'Blindtarmen'}
+    </Text>
   );
-}
+};
 
 export default Logo;
