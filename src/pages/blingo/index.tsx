598b2bf8cb705c2081d0f7fdcd6de5597b7c66cb
CodeyBoi <hannes.ryberg00@gmail.com>
2023-06-27 22:06:32 +0200

    Added key to blingo map

--- src/pages/blingo/index.tsx
+++ src/pages/blingo/index.tsx
@@ -1,4 +1,3 @@
-
 import { Button, Checkbox, Grid, SimpleGrid } from '@mantine/core';
 import React from 'react';
 import BingoTile from '../../components/blingo/tile';
@@ -6,26 +5,22 @@ import { trpc } from '../../utils/trpc';
 import BingoEntryForm from '../../components/blingo/entry-form';
 
 const Bingo = () => {
-
   const utils = trpc.useContext();
 
   const { data: card } = trpc.bingo.getCard.useQuery();
   const mutation = trpc.bingo.generateCard.useMutation({
     onSuccess: () => {
       utils.bingo.getCard.invalidate();
-    }
+    },
   });
 
   return (
     <div>
       <SimpleGrid cols={5}>
         {card?.entries.map((entry) => (
-          <BingoTile text={entry.text}></BingoTile>
+          <BingoTile key={entry.id} text={entry.text}></BingoTile>
         ))}
-
-
       </SimpleGrid>
-
 
       <BingoEntryForm></BingoEntryForm>
       {!card && (
@@ -34,10 +29,7 @@ const Bingo = () => {
         </Button>
       )}
     </div>
-
-
   );
-
-}
+};
 
 export default Bingo;
