7826e37c26c96e291a0fb317cee6d5f1c55c61db
Aron <aronpaulsson99@gmail.com>
2023-07-05 22:40:55 +0200

    Forgot to re add statistik för nördar button, WARNING requires @mui/materials and @mui/x-data-grid

--- src/components/statistics-table.tsx
+++ src/components/statistics-table.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { DataGrid, GridColDef} from 'sudo ap';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { ThemeProvider, createTheme } from '@mui/material';
+import { Button, Stack, Text } from '@mantine/core';
+import { NextLink } from '@mantine/next';
+import { IconMoodNerd } from '@tabler/icons';
 import { trpc } from '../utils/trpc';
 import AlertError from './alert-error';
 import Loading from './loading';
@@ -19,12 +22,12 @@ const theme = createTheme({
 });
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'id'},
+  { field: 'id', headerName: 'id', width: 0 },
   { field: 'number', headerName: '#', width: 55 },
   { field: 'name', headerName: 'Namn', flex: 1 },
-  { field: 'points', headerName: 'Poäng', width: 91 },
+  { field: 'points', headerName: 'Poäng', type: 'number', width: 91 },
   { field: 'attendance', headerName: 'Närvaro', width: 100 },
-  { field: 'totalPoints', headerName: 'Totala Poäng', width: 140 },
+  { field: 'totalPoints', headerName: 'Totala Poäng', type: 'number', width: 150 },
 ];
 
 const StatisticsTable = ({ start, end }: StatisticsTableProps) => {
@@ -46,7 +49,8 @@ const StatisticsTable = ({ start, end }: StatisticsTableProps) => {
 
   const nbrOfGigsString = nbrOfGigs !== 0
     ? `Denna period ${isNow ? 'har vi hittills haft' : 'hade vi'} ${nbrOfGigs} spelning${nbrOfGigs === 1 ? '' : 'ar'}` : '';
-
+  const positiveGigsString = (positivelyCountedGigs ?? 0) > 0
+    ? `, där ${positivelyCountedGigs} ${isNow ? 'räknats' : 'räknades'} positivt.` : '.';
   const ownPoints = corps && stats ? stats.corpsStats[corps.id]?.gigsAttended : undefined;
   const ownAttendence = corps && stats ? stats.corpsStats[corps.id]?.attendence : undefined;
   // Somehow ownPoints is a string, so == is used instead of ===setSortColumn
@@ -64,41 +68,55 @@ const StatisticsTable = ({ start, end }: StatisticsTableProps) => {
     }
     return bStat.attendence - aStat.attendence;
   });
-  if (corpsIdsSorted === undefined) {
+  if (corpsIdsSorted === undefined || corpsIds === undefined) {
     return <AlertError msg='Kunde inte hämta spelningsstatistik.' />;
   }
 
   const rows = corpsIdsSorted.map((corpsId) => {
     const stat = corpsStats[corpsId];
     if (!stat) return null;
-    return{
+    return {
       id: stat.id,
       number: stat.number ?? 'p.e.',
       name: `${stat.firstName} ${stat.lastName}`,
-      points: `${stat.gigsAttended}`,
+      points: stat.gigsAttended,
       attendance: `${Math.round(stat.attendence * 100)}%`,
-      totalPoints: `${corpsPoints.points[corpsId]}`
+      totalPoints: corpsPoints.points[corpsId]
     };
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div style={{ height: '100%', width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          hideFooterPagination
-          hideFooterSelectedRowCount
-          disableColumnMenu
-          disableColumnFilter
-          disableColumnSelector
-          columnVisibilityModel={{
-            id: false,
-          }}
-
-        />
-      </div>
-    </ThemeProvider>
+    <>{corpsIds && corpsIds.length === 0 && (<Text>Det finns inga statistikuppgifter för denna period.</Text>)}
+      {corpsPoints && corpsStats && corpsIdsSorted && corpsIdsSorted.length !== 0 && (
+        <Stack>
+          <Text>
+            {nbrOfGigsString + (nbrOfGigs !== 0 ? positiveGigsString : '')}
+          </Text>
+          {ownPointsString && <Text>{ownPointsString}</Text>}
+          <ThemeProvider theme={theme}>
+            <div style={{ height: '100%', width: '100%' }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                hideFooterPagination
+                hideFooterSelectedRowCount
+                disableColumnMenu
+                disableColumnFilter
+                disableColumnSelector
+                columnVisibilityModel={{
+                  id: false,
+                }}
+              />
+            </div>
+          </ThemeProvider>
+          <Button
+            component={NextLink}
+            href='/stats/for/nerds'
+            leftIcon={<IconMoodNerd />}
+          >
+            Statistik för nördar
+          </Button>
+        </Stack>)}</>
   );
 }
 
