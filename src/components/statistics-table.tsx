4287fa51f6193e9fb81b0c121eb7478549f86bdc
CodeyBoi <hannes.ryberg00@gmail.com>
2023-07-09 23:17:20 +0200

    Added dependency

--- src/components/statistics-table.tsx
+++ src/components/statistics-table.tsx
@@ -11,7 +11,7 @@ import Loading from './loading';
 interface StatisticsTableProps {
   start: Date;
   end: Date;
-};
+}
 
 const theme = createTheme({
   palette: {
@@ -27,14 +27,26 @@ const columns: GridColDef[] = [
   { field: 'name', headerName: 'Namn', flex: 1 },
   { field: 'points', headerName: 'Poäng', type: 'number', width: 91 },
   { field: 'attendance', headerName: 'Närvaro', width: 100 },
-  { field: 'totalPoints', headerName: 'Totala Poäng', type: 'number', width: 150 },
+  {
+    field: 'totalPoints',
+    headerName: 'Totala poäng',
+    type: 'number',
+    width: 150,
+  },
 ];
 
 const StatisticsTable = ({ start, end }: StatisticsTableProps) => {
-  const { data: stats, status: statsStatus } = trpc.stats.get.useQuery({ start, end, });
-  const { nbrOfGigs, positivelyCountedGigs, corpsStats, corpsIds } = stats ?? {};
+  const { data: stats, status: statsStatus } = trpc.stats.get.useQuery({
+    start,
+    end,
+  });
+  const { nbrOfGigs, positivelyCountedGigs, corpsStats, corpsIds } =
+    stats ?? {};
   const { data: corps } = trpc.corps.getSelf.useQuery();
-  const { data: corpsPoints } = trpc.stats.getManyPoints.useQuery({ corpsIds }, { enabled: !!corpsIds });
+  const { data: corpsPoints } = trpc.stats.getManyPoints.useQuery(
+    { corpsIds },
+    { enabled: !!corpsIds },
+  );
 
   if (!corpsStats || !corpsPoints) {
     return <Loading msg='Laddar statistik...' />;
@@ -47,23 +59,39 @@ const StatisticsTable = ({ start, end }: StatisticsTableProps) => {
   today.setHours(0, 0, 0, 0);
   const isNow = today <= end;
 
-  const nbrOfGigsString = nbrOfGigs !== 0
-    ? `Denna period ${isNow ? 'har vi hittills haft' : 'hade vi'} ${nbrOfGigs} spelning${nbrOfGigs === 1 ? '' : 'ar'}` : '';
-  const positiveGigsString = (positivelyCountedGigs ?? 0) > 0
-    ? `, där ${positivelyCountedGigs} ${isNow ? 'räknats' : 'räknades'} positivt.` : '.';
-  const ownPoints = corps && stats ? stats.corpsStats[corps.id]?.gigsAttended : undefined;
-  const ownAttendence = corps && stats ? stats.corpsStats[corps.id]?.attendence : undefined;
+  const nbrOfGigsString =
+    nbrOfGigs !== 0
+      ? `Denna period ${
+          isNow ? 'har vi hittills haft' : 'hade vi'
+        } ${nbrOfGigs} spelning${nbrOfGigs === 1 ? '' : 'ar'}`
+      : '';
+  const positiveGigsString =
+    (positivelyCountedGigs ?? 0) > 0
+      ? `, där ${positivelyCountedGigs} ${
+          isNow ? 'räknats' : 'räknades'
+        } positivt.`
+      : '.';
+  const ownPoints =
+    corps && stats ? stats.corpsStats[corps.id]?.gigsAttended : undefined;
+  const ownAttendence =
+    corps && stats ? stats.corpsStats[corps.id]?.attendence : undefined;
   // Somehow ownPoints is a string, so == is used instead of ===setSortColumn
-  const ownPointsString = ownPoints && ownAttendence && nbrOfGigs !== 0
-    ? `Du ${isNow ? 'har varit' : 'var'} med på ${ownPoints} spelning${ownPoints == 1 ? '' : 'ar'
-    }, vilket ${isNow ? 'motsvarar' : 'motsvarade'} ${Math.round(ownAttendence * 100,)}% närvaro.`
-    : undefined;
+  const ownPointsString =
+    ownPoints && ownAttendence && nbrOfGigs !== 0
+      ? `Du ${isNow ? 'har varit' : 'var'} med på ${ownPoints} spelning${
+          ownPoints == 1 ? '' : 'ar'
+        }, vilket ${isNow ? 'motsvarar' : 'motsvarade'} ${Math.round(
+          ownAttendence * 100,
+        )}% närvaro.`
+      : undefined;
 
   const corpsIdsSorted = corpsIds?.sort((a, b) => {
     const aStat = corpsStats[a];
     const bStat = corpsStats[b];
     if (!aStat || !bStat) return 0;
-    if (Math.ceil(aStat.attendence * 100) === Math.ceil(bStat.attendence * 100)) {
+    if (
+      Math.ceil(aStat.attendence * 100) === Math.ceil(bStat.attendence * 100)
+    ) {
       return bStat.gigsAttended - aStat.gigsAttended;
     }
     return bStat.attendence - aStat.attendence;
@@ -81,43 +109,51 @@ const StatisticsTable = ({ start, end }: StatisticsTableProps) => {
       name: `${stat.firstName} ${stat.lastName}`,
       points: stat.gigsAttended,
       attendance: `${Math.round(stat.attendence * 100)}%`,
-      totalPoints: corpsPoints.points[corpsId]
+      totalPoints: corpsPoints.points[corpsId],
     };
   });
 
   return (
-    <>{corpsIds && corpsIds.length === 0 && (<Text>Det finns inga statistikuppgifter för denna period.</Text>)}
-      {corpsPoints && corpsStats && corpsIdsSorted && corpsIdsSorted.length !== 0 && (
-        <Stack>
-          <Text>
-            {nbrOfGigsString + (nbrOfGigs !== 0 ? positiveGigsString : '')}
-          </Text>
-          {ownPointsString && <Text>{ownPointsString}</Text>}
-          <ThemeProvider theme={theme}>
-            <div style={{ height: '100%', width: '100%' }}>
-              <DataGrid
-                rows={rows}
-                columns={columns}
-                hideFooterPagination
-                hideFooterSelectedRowCount
-                disableColumnMenu
-                disableColumnFilter
-                disableColumnSelector
-                columnVisibilityModel={{
-                  id: false,
-                }}
-              />
-            </div>
-          </ThemeProvider>
-          <Button
-            component={NextLink}
-            href='/stats/for/nerds'
-            leftIcon={<IconMoodNerd />}
-          >
-            Statistik för nördar
-          </Button>
-        </Stack>)}</>
+    <>
+      {corpsIds && corpsIds.length === 0 && (
+        <Text>Det finns inga statistikuppgifter för denna period.</Text>
+      )}
+      {corpsPoints &&
+        corpsStats &&
+        corpsIdsSorted &&
+        corpsIdsSorted.length !== 0 && (
+          <Stack>
+            <Text>
+              {nbrOfGigsString + (nbrOfGigs !== 0 ? positiveGigsString : '')}
+            </Text>
+            {ownPointsString && <Text>{ownPointsString}</Text>}
+            <ThemeProvider theme={theme}>
+              <div style={{ height: '100%', width: '100%' }}>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  hideFooterPagination
+                  hideFooterSelectedRowCount
+                  disableColumnMenu
+                  disableColumnFilter
+                  disableColumnSelector
+                  columnVisibilityModel={{
+                    id: false,
+                  }}
+                />
+              </div>
+            </ThemeProvider>
+            <Button
+              component={NextLink}
+              href='/stats/for/nerds'
+              leftIcon={<IconMoodNerd />}
+            >
+              Statistik för nördar
+            </Button>
+          </Stack>
+        )}
+    </>
   );
-}
+};
 
 export default StatisticsTable;
