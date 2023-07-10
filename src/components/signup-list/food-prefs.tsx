1bf05ab949a7fd8badd085aac712788e546f6d16
CodeyBoi <hannes.ryberg00@gmail.com>
2023-07-10 14:40:44 +0200

    Added button to download food prefs as CSV

--- src/components/signup-list/food-prefs.tsx
+++ src/components/signup-list/food-prefs.tsx
@@ -1,7 +1,8 @@
 import { CorpsFoodPrefs } from '@prisma/client';
 import React from 'react';
 import { trpc } from '../../utils/trpc';
-import { Table, createStyles } from '@mantine/core';
+import { Button, Space, Table, createStyles } from '@mantine/core';
+import { IconDownload } from '@tabler/icons';
 
 type FoodPrefsProps = {
   gigId: string;
@@ -25,48 +26,93 @@ const useStyles = createStyles(() => ({
 }));
 
 const FoodPrefs = ({ gigId, foodPrefs }: FoodPrefsProps) => {
+  const { data: gig } = trpc.gig.getWithId.useQuery({ gigId });
   const { data: signups } = trpc.gig.getSignups.useQuery({ gigId });
   const { classes } = useStyles();
   const yesSignups = signups?.filter((signup) => signup.signupStatus === 'Ja');
-  const rows = yesSignups?.map((signup) => {
+
+  const dataRows = yesSignups?.map((signup) => {
     const corpsPrefs = foodPrefs[signup.corpsId] as CorpsFoodPrefs;
     const prefs = corpsPrefs ? corpsPrefs : DEFAULT_FOOD_PREFS;
 
-    return (
-      <tr key={signup.corpsId}>
-        <td>{`${signup.number ? '#' + signup.number.toString() : 'p.e.'} ${
+    return {
+      corpsId: signup.corpsId,
+      data: [
+        `${signup.number ? '#' + signup.number.toString() : 'p.e.'} ${
           signup.firstName
-        } ${signup.lastName}`}</td>
-        <td>{prefs.vegetarian ? 'X' : ''}</td>
-        <td>{prefs.vegan ? 'X' : ''}</td>
-        <td>{prefs.glutenFree ? 'X' : ''}</td>
-        <td>{prefs.lactoseFree ? 'X' : ''}</td>
-        <td>{prefs.drinksAlcohol ? 'X' : ''}</td>
-        <td>{prefs.other}</td>
+        } ${signup.lastName}`,
+        prefs.vegetarian ? 'X' : '',
+        prefs.vegan ? 'X' : '',
+        prefs.glutenFree ? 'X' : '',
+        prefs.lactoseFree ? 'X' : '',
+        prefs.drinksAlcohol ? 'X' : '',
+        prefs.other,
+      ],
+    };
+  });
+
+  const csvDownloadLink = dataRows
+    ? `data:text/csv;charset=utf-8,Namn,Vegetarian,Vegan,Gluten,Laktos,Alkohol,Övrigt\n${encodeURIComponent(
+        dataRows
+          ?.map((row) =>
+            row.data
+              // Wrap text in quotes, otherwise it will be split into multiple columns if it contains commas
+              .map((text) => `"${text}"`)
+              .join(','),
+          )
+          .join('\n') ?? '',
+      )}`
+    : '';
+
+  const rows = dataRows?.map((row) => {
+    return (
+      <tr key={row.corpsId}>
+        <td>{row.data[0]}</td>
+        <td>{row.data[1]}</td>
+        <td>{row.data[2]}</td>
+        <td>{row.data[3]}</td>
+        <td>{row.data[4]}</td>
+        <td>{row.data[5]}</td>
+        <td>{row.data[6]}</td>
       </tr>
     );
   });
 
   return (
-    <Table
-      withColumnBorders
-      horizontalSpacing={2}
-      verticalSpacing={2}
-      className={classes.table}
-    >
-      <thead>
-        <tr>
-          <th>Namn</th>
-          <th>Vegetarian</th>
-          <th>Vegan</th>
-          <th>Gluten</th>
-          <th>Laktos</th>
-          <th>Alkohol</th>
-          <th>Övrigt</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+    <>
+      <Table
+        withColumnBorders
+        horizontalSpacing={2}
+        verticalSpacing={2}
+        className={classes.table}
+      >
+        <thead>
+          <tr>
+            <th>Namn</th>
+            <th>Vegetarian</th>
+            <th>Vegan</th>
+            <th>Gluten</th>
+            <th>Laktos</th>
+            <th>Alkohol</th>
+            <th>Övrigt</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+      {dataRows && (
+        <>
+          <Space h='md' />
+          <Button
+            leftIcon={<IconDownload />}
+            component={'a'}
+            href={csvDownloadLink}
+            download={`Matpreffar ${gig?.title.trim() ?? ''}.csv`}
+          >
+            Ladda ner som CSV
+          </Button>
+        </>
+      )}
+    </>
   );
 };
 
