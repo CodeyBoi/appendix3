997bc4476417b9b7ca9f9d797e3f17b14862d47c
CodeyBoi <hannes.ryberg00@gmail.com>
2023-07-27 16:10:51 +0200

    Hotfixed allowed origins

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -13,8 +13,8 @@ export const router = t.router;
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://bleckhornen.org',
-  'https://bleckhornen.org',
+  'http://www.bleckhornen.org',
+  'https://www.bleckhornen.org',
   'http://testing.bleckhornen.org',
   'https://testing.bleckhornen.org',
 ];
