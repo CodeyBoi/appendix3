e2bf50b1100f8dd79ac0aa5780aa41b0507cb972
CodeyBoi <hannes.ryberg00@gmail.com>
2023-10-20 10:29:52 +0200

    Entire gigcard is now clickable

--- src/components/gig/card.tsx
+++ src/components/gig/card.tsx
@@ -41,28 +41,26 @@ const GigCard = ({ gig }: GigCardProps) => {
       <div className='border rounded shadow-md dark:border-neutral-800'>
         <div className='flex flex-col p-4 space-y-2'>
           <div className='flex items-start justify-between flex-nowrap'>
-            <Link href={`/gig/${gig.id}`}>
+            <Link className='flex-grow' href={`/gig/${gig.id}`}>
               <h4 className='cursor-pointer'>{gig.title}</h4>
             </Link>
             <GigMenu gig={gig} isAdmin={isAdmin} setOpened={setOpened} />
           </div>
           <div className='flex flex-col justify-between lg:flex-row'>
-            <div className='flex flex-grow'>
-              <Link href={`/gig/${gig.id}`}>
-                <div className='flex flex-row items-center flex-grow space-x-4 cursor-pointer'>
-                  <Datebox date={dayjs(gig.date)} />
-                  <div className='text-xs leading-normal'>
-                    <i>{gig.type.name}</i>
-                    <br />
-                    {!!gig.location && `${gig.location}`}
-                    {!!gig.location && <br />}
-                    {!!gig.meetup && `Samling: ${gig.meetup}`}
-                    {!!gig.meetup && <br />}
-                    {!!gig.start && `Spelstart: ${gig.start}`}
-                  </div>
+            <Link className='flex-grow' href={`/gig/${gig.id}`}>
+              <div className='flex items-center flex-grow space-x-4 cursor-pointer'>
+                <Datebox date={dayjs(gig.date)} />
+                <div className='text-xs leading-normal'>
+                  <i>{gig.type.name}</i>
+                  <br />
+                  {!!gig.location && `${gig.location}`}
+                  {!!gig.location && <br />}
+                  {!!gig.meetup && `Samling: ${gig.meetup}`}
+                  {!!gig.meetup && <br />}
+                  {!!gig.start && `Spelstart: ${gig.start}`}
                 </div>
-              </Link>
-            </div>
+              </div>
+            </Link>
             <div className='w-full lg:w-56'>
               {showSignup && (
                 <GigSignupBox
