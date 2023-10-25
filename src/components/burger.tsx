9e73892299b6dca014aef6201d962689c8eba328
CodeyBoi <hannes.ryberg00@gmail.com>
2023-10-25 20:35:44 +0200

    Updated burger button

--- src/components/burger.tsx
+++ src/components/burger.tsx
@@ -10,20 +10,24 @@ const Burger = ({
   };
 
   return (
-    <button className='flex flex-col gap-1 p-3' onClick={handleClick}>
+    <button className='flex flex-col gap-0.5 p-3' onClick={handleClick}>
       <div
-        className={`w-5 h-0.5 bg-white rounded-full transform transition-transform ${
-          active ? 'rotate-45 translate-y-1.5' : ''
+        className={`w-6 h-1 transition-all ${
+          active
+            ? 'rotate-45 translate-y-1.5 border-0 bg-white rounded-full'
+            : 'bg-amber-300 rounded-t-full'
         }`}
       />
       <div
-        className={`w-5 h-0.5 bg-white rounded-full transform transition-all ${
+        className={`w-5 ml-0.5 h-1 bg-orange-950 border-t border-t-yellow-400 rounded-full transform transition-all ${
           active ? 'opacity-0' : ''
         }`}
       />
       <div
-        className={`w-5 h-0.5 bg-white rounded-full transform transition-transform ${
-          active ? '-rotate-45 -translate-y-1.5' : ''
+        className={`w-6 h-1 transition-all ${
+          active
+            ? '-rotate-45 -translate-y-1.5 border-0 bg-white rounded-full'
+            : 'border-t-2 border-t-green-600 bg-amber-300 rounded-b-sm'
         }`}
       />
     </button>
