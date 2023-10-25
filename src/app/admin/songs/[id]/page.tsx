f6e08173a5ccb5582ba7d200e8cbd247d4102f02
CodeyBoi <hannes.ryberg00@gmail.com>
2023-10-25 17:37:53 +0200

    SongForm is now server component

---
+++ src/app/admin/songs/[id]/page.tsx
@@ -0,0 +1,21 @@
+import SongForm from 'components/song/form';
+import { api } from 'trpc/server';
+
+const Song = async ({ searchParams }: { searchParams: { id: string } }) => {
+  const id = searchParams.id;
+  const newSong = id === 'new';
+  const song = !newSong ? await api.song.get.query({ id }) : undefined;
+
+  if (song === null) {
+    return <h3>Sången finns inte.</h3>;
+  }
+
+  return (
+    <div className='flex flex-col max-w-sm'>
+      <h2>{(newSong ? 'Skapa' : 'Uppdatera') + ' sång'}</h2>
+      <SongForm song={song} />
+    </div>
+  );
+};
+
+export default Song;
--- src/components/song/form.tsx
+++ src/components/song/form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useForm } from '@mantine/form';
 import { useRouter } from 'next/router';
 import { trpc } from '../../utils/trpc';
