2172c4eab3973558ffff61a4cff21aa3bb888aa6
CodeyBoi <hannes.ryberg00@gmail.com>
2023-10-27 13:06:37 +0200

    Converted Datebox to tailwind

--- src/components/gig/datebox.tsx
+++ src/components/gig/datebox.tsx
@@ -1,23 +1,29 @@
-import { Dayjs } from "dayjs";
-import React from "react";
-import styles from "./info.module.css";
+import { Dayjs } from 'dayjs';
+import React from 'react';
 
 interface DateboxProps {
   date: Dayjs;
 }
 
 const Datebox = ({ date }: DateboxProps) => (
-  <div className={styles.datebox}>
-    <div className={styles.datemonth}>
-      <div className={styles.month}>
-        {date.toDate().toLocaleDateString('sv-SE', { month: 'short' }).slice(0, 3).toUpperCase()}
-      </div>
-      <div className={styles.date}>
-        {date.date()}
-      </div>
+  <div
+    className='flex flex-col w-16 text-center border border-black rounded h-min'
+    style={{ boxShadow: '2px 3px #888888' }}
+  >
+    <div className='pt-0.5 text-xs text-white uppercase font-light bg-red-600'>
+      {date
+        .toDate()
+        .toLocaleDateString('sv-SE', { month: 'short' })
+        .slice(0, 3)}
     </div>
-    <div className={styles.day}>
-      {date.toDate().toLocaleDateString('sv-SE', { weekday: 'short' }).slice(0, 3).toUpperCase()}
+    <div className='text-4xl text-white bg-red-600 font-castelar'>
+      {date.date()}
+    </div>
+    <div className='text-black uppercase bg-neutral-300'>
+      {date
+        .toDate()
+        .toLocaleDateString('sv-SE', { weekday: 'short' })
+        .slice(0, 3)}
     </div>
   </div>
 );
