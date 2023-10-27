db117af1626d205ecb76af45cdf99daf04141d0b
CodeyBoi <hannes.ryberg00@gmail.com>
2023-10-27 13:21:07 +0200

    Removed unused packages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { ServerStyles, createStylesServer } from '@mantine/next';
 import Document, {
   DocumentContext,
   Head,
@@ -6,22 +5,13 @@ import Document, {
   NextScript,
 } from 'next/document';
 
-const stylesServer = createStylesServer();
-
 export default class _Document extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
     return {
       ...initialProps,
-      styles: [
-        initialProps.styles,
-        <ServerStyles
-          html={initialProps.html}
-          server={stylesServer}
-          key='styles'
-        />,
-      ],
+      styles: [initialProps.styles],
     };
   }
 
