d86035f85883e235dbd861aede978c07d6cf1186
CodeyBoi <hannes.ryberg00@gmail.com>
2023-11-08 17:41:49 +0100

    Added slagverk animation

--- src/hooks/easter-eggs.ts
+++ src/hooks/easter-eggs.ts
@@ -1,18 +1,9 @@
 import useKeySequence from 'hooks/use-key-sequence';
 
-const setEnabled = (enabled: boolean) => {
-  if (typeof window === 'undefined') {
-    return;
-  }
-  if (enabled) {
-    document.documentElement.classList.add('slagverk');
-  } else {
-    document.documentElement.classList.remove('slagverk');
-  }
-};
-
 export const useSlagverkEasterEgg = () => {
-  useKeySequence(['s', 'l', 'a', 'g', 'v', 'e', 'r', 'k'], () =>
-    setEnabled(true),
-  );
+  useKeySequence(['s', 'l', 'a', 'g', 'v', 'e', 'r', 'k'], () => {
+    if (typeof window !== 'undefined') {
+      document.documentElement.classList.add('slagverk');
+    }
+  });
 };
