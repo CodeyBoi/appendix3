1a6e3d1b96aa3fcb26d48c318b34979e85e81efd
CodeyBoi <hannes.ryberg00@gmail.com>
2023-11-21 11:48:15 +0100

    DateTimePicker working

--- src/components/input/datetime-picker/index.tsx
+++ src/components/input/datetime-picker/index.tsx
@@ -1,4 +1,4 @@
-import { IconCalendar } from '@tabler/icons-react';
+import { IconCalendar, IconX } from '@tabler/icons-react';
 import Popover from 'components/popover';
 import dayjs from 'dayjs';
 import { useState } from 'react';
@@ -7,12 +7,13 @@ import DatePickerDropdown from '../date-picker/dropdown';
 import TimePickerDropdown from './time-picker-dropdown';
 
 type DateTimePickerProps = {
-  value?: Date;
-  onChange?: (date: Date) => void;
+  value?: Date | null;
+  onChange?: (date: Date | null) => void;
   label?: string;
   withAsterisk?: boolean;
   placeholder?: string;
-  format?: string;
+  defaultValue?: Date;
+  clearable?: boolean;
 };
 
 const DateTimePicker = ({
@@ -21,12 +22,13 @@ const DateTimePicker = ({
   label,
   withAsterisk,
   placeholder,
-  format = 'YYYY-MM-DD HH:mm',
+  defaultValue,
+  clearable = false,
 }: DateTimePickerProps) => {
   const [date, setDate] = useState<Date | null>(null);
 
   const handleDateChange = (d: Date) => {
-    const newDate = date ? new Date(date) : new Date();
+    const newDate = date ? new Date(date) : defaultValue ?? new Date();
     newDate.setFullYear(d.getFullYear());
     newDate.setMonth(d.getMonth());
     newDate.setDate(d.getDate());
@@ -35,7 +37,7 @@ const DateTimePicker = ({
   };
 
   const handleTimeChange = (d: Date) => {
-    const newDate = date ? new Date(date) : new Date();
+    const newDate = date ? new Date(date) : defaultValue ?? new Date();
     newDate.setHours(d.getHours());
     newDate.setMinutes(d.getMinutes());
     setDate(newDate);
@@ -43,48 +45,52 @@ const DateTimePicker = ({
   };
 
   return (
-    <>
-      <input
-        hidden
-        type='datetime-local'
-        value={(value ?? date)?.toISOString()}
-      />
-      <Popover
-        position='left-bottom'
-        target={
-          <div className='relative mt-2 flex h-10 rounded border shadow-sm dark:border-neutral-800'>
-            <input
-              className='absolute h-full w-full grow bg-transparent px-3 pb-1 pt-3 text-left'
-              type='text'
-              value={value ? dayjs(value).format(format) : undefined}
-              placeholder={placeholder}
-              readOnly
-            />
-            {label && (
-              <label className='absolute flex -translate-y-2.5 translate-x-2 items-center justify-between bg-white px-1 text-sm text-neutral-500 dark:bg-darkBg dark:text-neutral-500'>
-                <div className='flex gap-1'>
-                  {label}
-                  {withAsterisk && (
-                    <span className='text-base text-red-600'>*</span>
-                  )}
-                </div>
-              </label>
-            )}
-            <div className='absolute right-0 flex h-10 w-10 items-center justify-center'>
-              <ActionIcon>
-                <IconCalendar />
+    <Popover
+      position='bottom-right'
+      target={
+        <div className='relative mt-2 flex h-10 rounded border shadow-sm dark:border-neutral-800'>
+          <input
+            className='absolute h-full w-full grow cursor-pointer bg-transparent px-3 pb-1 pt-3 text-left'
+            type='text'
+            value={value ? dayjs(value).format('YYYY-MM-DD HH:mm') : ''}
+            placeholder={placeholder}
+            readOnly
+          />
+          {label && (
+            <label className='absolute flex -translate-y-2.5 translate-x-2 items-center justify-between bg-white px-1 text-sm text-neutral-500 dark:bg-darkBg dark:text-neutral-500'>
+              <div className='flex gap-1'>
+                {label}
+                {withAsterisk && (
+                  <span className='text-base text-red-600'>*</span>
+                )}
+              </div>
+            </label>
+          )}
+          {clearable && value && (
+            <div className='absolute right-10 flex h-10 items-center justify-center'>
+              <ActionIcon
+                variant='subtle'
+                onClick={() => {
+                  onChange?.(null);
+                  setDate(null);
+                }}
+              >
+                <IconX />
               </ActionIcon>
             </div>
+          )}
+          <div className='absolute right-0 flex h-10 w-10 items-center justify-center text-red-600'>
+            <IconCalendar />
           </div>
-        }
-        popover={
-          <div className='flex gap-2'>
-            <DatePickerDropdown onDateChange={handleDateChange} />
-            <TimePickerDropdown onChange={handleTimeChange} />
-          </div>
-        }
-      />
-    </>
+        </div>
+      }
+      popover={
+        <div className='flex gap-2'>
+          <DatePickerDropdown onDateChange={handleDateChange} />
+          <TimePickerDropdown onChange={handleTimeChange} />
+        </div>
+      }
+    />
   );
 };
 
--- src/components/input/datetime-picker/time-picker-dropdown.tsx
+++ src/components/input/datetime-picker/time-picker-dropdown.tsx
@@ -1,4 +1,4 @@
-import ActionIcon from '../action-icon';
+import { cn } from 'utils/class-names';
 import { useState } from 'react';
 
 type TimePickerDropdownProps = {
@@ -12,44 +12,59 @@ const TimePickerDropdown = (props: TimePickerDropdownProps) => {
   const [hour, setHour] = useState(props.initialHour ?? 0);
   const [minute, setMinute] = useState(props.initialMinute ?? 0);
   return (
-    <div className='flex rounded bg-white p-2 dark:bg-darkBg'>
-      <div className='flex max-h-60 flex-col overflow-y-auto no-scrollbar'>
+    <div className='flex gap-1 rounded bg-white p-2 dark:bg-darkBg'>
+      <div className='flex max-h-52 flex-col overflow-y-auto no-scrollbar'>
         {Array.from({ length: 24 }).map((_, i) => {
           const h = i.toString().padStart(2, '0');
           return (
-            <div key={h} className='flex items-center'>
-              <ActionIcon
-                variant='subtle'
-                onClick={() => {
-                  const newDate = new Date(i, minute);
-                  onChange?.(newDate);
-                  setHour(i);
-                }}
-              >
-                <h4 className='select-none whitespace-nowrap'>{h}</h4>
-              </ActionIcon>
+            <div
+              key={h + 'h'}
+              className={cn(
+                'flex w-8 cursor-pointer justify-center rounded',
+                i === hour
+                  ? 'bg-red-600 text-white'
+                  : 'text-red-600 hover:bg-red-600/5',
+              )}
+              onClick={(e) => {
+                const date = new Date(0, 0, 0, i, minute, 0, 0);
+                onChange?.(date);
+                setHour(i);
+                e.currentTarget.scrollIntoView();
+              }}
+            >
+              <h4 className='select-none whitespace-nowrap'>{h}</h4>
             </div>
           );
         })}
-        <div className='flex max-h-60 flex-col overflow-y-auto no-scrollbar'>
-          {Array.from({ length: 12 }).map((_, i) => {
-            const m = (i * 5).toString().padStart(2, '0');
-            return (
-              <div key={m} className='flex items-center'>
-                <ActionIcon
-                  variant='subtle'
-                  onClick={() => {
-                    const newDate = new Date(hour, i * 5);
-                    onChange?.(newDate);
-                    setMinute(i * 5);
-                  }}
-                >
-                  <h4 className='select-none whitespace-nowrap'>{m}</h4>
-                </ActionIcon>
-              </div>
-            );
-          })}
-        </div>
+        {/* Add padding to the bottom so the last item can scroll to the top */}
+        <div className='block h-48' />
+      </div>
+      <div className='flex max-h-52 flex-col overflow-y-auto no-scrollbar'>
+        {Array.from({ length: 12 }).map((_, i) => {
+          const m = i * 5;
+          const mString = m.toString().padStart(2, '0');
+          return (
+            <div
+              key={mString + 'm'}
+              className={cn(
+                'flex w-8 cursor-pointer justify-center rounded',
+                m === minute
+                  ? 'bg-red-600 text-white'
+                  : 'text-red-600 hover:bg-red-600/5',
+              )}
+              onClick={(e) => {
+                const date = new Date(0, 0, 0, hour, m, 0, 0);
+                onChange?.(date);
+                setMinute(m);
+                e.currentTarget.scrollIntoView();
+              }}
+            >
+              <h4 className='select-none whitespace-nowrap'>{mString}</h4>
+            </div>
+          );
+        })}
+        {/* Add padding to the bottom so the last item can scroll to the top */}
+        <div className='h-48 w-px' />
       </div>
     </div>
   );
