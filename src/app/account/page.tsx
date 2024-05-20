c22b29af8018671b26ee871fff8464817e8c562b
Hannes Ryberg <47188830+CodeyBoi@users.noreply.github.com>
2024-05-20 17:03:13 +0200

    Update page.tsx

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -25,7 +25,7 @@ const Account = async () => {
         {`${corps ? ', ' + corpsName : ''}!`}
       </h2>
       <div className='grid grid-cols-1 gap-2 lg:grid-cols-2'>
-        <CorpsStats balance={streckAccount.balance} />
+        <CorpsStats />
         <AccountPreferences />
       </div>
     </div>
