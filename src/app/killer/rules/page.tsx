75893c23e594a536e79be541f89ad989d8c65676
Hannes Ryberg <47188830+CodeyBoi@users.noreply.github.com>
2024-11-19 22:12:26 +0100

    Updated killer rules (#235)

--- src/app/killer/rules/page.tsx
+++ src/app/killer/rules/page.tsx
@@ -6,30 +6,21 @@ const KillerRules = () => {
       <h3>{lang('Regler till Killergame', 'Rules for Killergame')}</h3>
       <div className='lang-sv flex max-w-4xl flex-col gap-2 text-left'>
         <p>
-          Varje spelare får vid start en (icke-fysisk) lapp med en annan
-          spelares namn och ett ord. Din uppgift är att &quot;döda&quot; corpset
-          på din lapp genom att få denne att säga ordet som också står på
-          lappen. När du har lyckats med detta visar du lappen för den du dödat
-          och får då deras lapp. På denna lapp står ditt nya offer. Den som har
-          flest mord när spelet tar slut vinner!
+          Varje spelare får vid start en (icke-fysisk) lapp med två andra
+          spelares namn. Din uppgift är att &quot;döda&quot; corpset på din lapp
+          genom att få denne att på något sätt hänvisa till den andra personen
+          som också står på lappen. När du lyckats med detta visar du lappen för
+          den du dödat och får då deras lapp. På denna lapp står ditt nya offer.
+          Den som har flest mord när spelet tar slut vinner!
         </p>
 
         <h4 className='mt-2'>Förtydliganden</h4>
         <ul className='list-disc pl-4'>
           <li>
-            Det räcker inte att höra personen säga ordet. Du måste vara i en
+            Det räcker inte att höra personen säga namnet. Du måste vara i en
             konversation med personen, antingen ensam eller i grupp.
           </li>
           <li>Mord får ske var som helst, när som helst, och hur som helst.</li>
-          <li>
-            Sammansatta ord som innehåller ordet gills. T.ex. hade{' '}
-            <i>tandborste</i> gillts om ordet var <i>tand</i>. Även böjda former
-            av ordet gills, t.ex. <i>tanden</i>. Synonymer gills dock inte!
-          </li>
-          <li>
-            Om ett ord har flera betydelser så är alla betydelser giltiga (inom
-            rimliga gränser).
-          </li>
           <li>Man får inte döda någon om man är död. (duh)</li>
         </ul>
         <h4 className='mt-2'>Övrigt</h4>
@@ -48,20 +39,17 @@ const KillerRules = () => {
       <div className='lang-en flex max-w-4xl flex-col gap-2 text-left'>
         <p>
           At the start of the game, each player receives a (non-physical) piece
-          of paper with another player&apos;s name and a word. Your task is to
+          of paper with two other player&apos;s names. Your task is to
           &quot;kill&quot; the corps on your note by getting them to say the
-          word that is also on the note. When you succeed, you show the note to
-          the person you killed and then get their note. On this note is your
-          new target. Whoever has the most notes when the game ends wins!
+          name of the other person that is also on the note. When you succeed,
+          you show the note to the person you killed and then get their note. On
+          this note is your new target. Whoever has the most notes when the game
+          ends wins!
         </p>
         <h4 className='mt-2'>Clarifications</h4>
         <ul className='list-disc pl-4'>
           <li>
-            If a word has multiple meanings, any meaning is valid (be
-            reasonable).
-          </li>
-          <li>
-            It is not enough to hear the person say the word. You must be in a
+            It is not enough to hear the person say the name. You must be in a
             conversation with the person, either alone or in a group.
           </li>
           <li>Murders may happen anywhere, anytime, in any way.</li>
@@ -70,7 +58,7 @@ const KillerRules = () => {
         <h4 className='mt-2'>Other</h4>
         <ul className='list-disc pl-4'>
           <li>
-            Register your kills in Blindtarmen by entering the word that was on
+            Register your kills in Blindtarmen by entering the name that was on
             the note your victim had. Try to do this as soon as possible after a
             confirmed kill, so that all participants have an updated list of who
             are still in the game.
