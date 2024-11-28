126af0fa5a493a11fc27abf8fb2bb6159dc1ea36
Hannes Ryberg <47188830+CodeyBoi@users.noreply.github.com>
2024-11-28 14:57:50 +0100

    Strecksaldo (#246)
    * Made a server version of Restricted component
    
    * Strecklist form can now handle monolists

--- src/app/admin/streck/view/[id]/form.tsx
+++ src/app/admin/streck/view/[id]/form.tsx
@@ -49,7 +49,7 @@ const rowBackgroundColor = (balance: number) => {
 const AdminStreckForm = ({
   id,
   transactions = [],
-  items,
+  items: propItems,
 }: AdminStreckFormProps) => {
   const router = useRouter();
   const utils = api.useUtils();
@@ -71,35 +71,52 @@ const AdminStreckForm = ({
     additionalCorps,
   });
 
-  const itemsSet = new Set(items.map((i) => `${i.name}:${i.price}`));
-  for (const transaction of transactions) {
-    const key = `${transaction.item}:${-transaction.pricePer}`;
-    if (itemsSet.has(key)) {
-      continue;
+  // If transactions has exactly one type of item, this is a one-time
+  // payment/deposit (aka. monolist). We handle monolists by only
+  // having one item, letting the user fill in price for each corps
+  // and fixing `amount` at 1.
+  const isMonoList =
+    new Set(transactions.map((transaction) => transaction.item)).size === 1;
+  const items: StreckItem[] = [];
+  if (isMonoList) {
+    const firstTransaction = transactions[0];
+    if (!firstTransaction) {
+      throw new Error(
+        'This should be unreachable as `isMonoList === true` implies `transactions` has a length of at least 1',
+      );
     }
-    itemsSet.add(key);
-    items.push({ name: transaction.item, price: -transaction.pricePer });
+    items.push({
+      name: firstTransaction.item,
+      price: NaN,
+    });
+  } else {
+    items.push(...propItems);
+    const itemsSet = new Set(items.map((i) => i.name));
+    for (const transaction of transactions) {
+      const key = transaction.item;
+      if (itemsSet.has(key)) {
+        continue;
+      }
+      itemsSet.add(key);
+      items.push({ name: transaction.item, price: -transaction.pricePer });
+    }
   }
 
-  const initialAmounts = transactions.reduce(
-    (acc, transaction) => {
-      const key = `${transaction.corps.id}:${
-        transaction.item
-      }:${-transaction.pricePer}`;
-      acc[key] = (acc[key] ?? 0) + transaction.amount;
-      return acc;
-    },
-    {} as Record<string, number>,
-  );
+  const initialAmounts = transactions.reduce((acc, transaction) => {
+    const key = `${transaction.corps.id}:${transaction.item}`;
+    acc.set(
+      key,
+      (acc.get(key) ?? 0) +
+        (isMonoList ? transaction.pricePer : transaction.amount),
+    );
+    return acc;
+  }, new Map<string, number>());
 
-  const initialBalances = transactions.reduce(
-    (acc, transaction) => {
-      const key = transaction.corps.id;
-      acc[key] = (acc[key] ?? 0) + transaction.totalPrice;
-      return acc;
-    },
-    {} as Record<string, number>,
-  );
+  const initialBalances = transactions.reduce((acc, transaction) => {
+    const key = transaction.corps.id;
+    acc.set(key, (acc.get(key) ?? 0) + transaction.totalPrice);
+    return acc;
+  }, new Map<string, number>());
 
   const mutation = api.streck.upsertStreckList.useMutation({
     onSuccess: () => {
@@ -114,16 +131,25 @@ const AdminStreckForm = ({
     const data = [];
     for (const corps of activeCorps) {
       for (const item of items) {
-        const formValue =
-          values[`${corps.id}:${item.name}:${item.price}`]?.trim();
+        const formValue = values[`${corps.id}:${item.name}`]?.trim();
         if (!formValue) {
           continue;
         }
+
+        // If `price` is NaN, user fills in price and `amount` is set to 1
+        const entry = isNaN(item.price)
+          ? {
+              amount: 1,
+              pricePer: +formValue,
+            }
+          : {
+              amount: +formValue,
+              pricePer: -item.price,
+            };
         data.push({
           corpsId: corps.id,
           item: item.name,
-          amount: +formValue,
-          pricePer: -item.price,
+          ...entry,
         });
       }
     }
@@ -135,7 +161,7 @@ const AdminStreckForm = ({
 
   return (
     <div className='flex flex-col gap-4'>
-      <h2>Lägg till strecklista</h2>
+      <h2>Inför strecklista</h2>
       <div className='max-w-md'>
         <SelectCorps
           label='Lägg till corps...'
@@ -150,14 +176,13 @@ const AdminStreckForm = ({
           <div className='overflow-x-auto'>
             <table className='table text-sm'>
               <thead>
-                <tr className='text-left align-bottom text-xs'>
+                <tr className='divide-x border-b text-left align-bottom text-xs'>
                   <th className='px-1'>Namn</th>
                   <th className='px-1'>Saldo</th>
                   {items.map((item) => (
-                    <th
-                      key={`${item.name}:${item.price}`}
-                      className='w-16 px-1'
-                    >{`${item.name} ${item.price}p`}</th>
+                    <th key={`${item.name}`} className='w-16 px-1'>{`${
+                      item.name
+                    } ${isNaN(item.price) ? '' : `${item.price}p`}`}</th>
                   ))}
                 </tr>
               </thead>
@@ -166,23 +191,23 @@ const AdminStreckForm = ({
                   <tr
                     key={corps.id}
                     className={`divide-x divide-solid dark:divide-neutral-800 ${rowBackgroundColor(
-                      corps.balance - (initialBalances[corps.id] ?? 0),
+                      corps.balance - (initialBalances.get(corps.id) ?? 0),
                     )}`}
                   >
                     <td className='px-1'>
                       <CorpsDisplay corps={corps} nameFormat='full-name' />
                     </td>
                     <td className='px-1 text-center'>
-                      {corps.balance - (initialBalances[corps.id] ?? 0)}
+                      {corps.balance - (initialBalances.get(corps.id) ?? 0)}
                     </td>
                     {items.map((item) => {
-                      const key = `${corps.id}:${item.name}:${item.price}`;
+                      const key = `${corps.id}:${item.name}`;
                       return (
                         <td key={key} className='px-1'>
                           <input
                             className='w-16 bg-transparent px-2 py-0.5'
                             type='number'
-                            defaultValue={initialAmounts[key]}
+                            defaultValue={initialAmounts.get(key)}
                             {...register(key)}
                           />
                         </td>
