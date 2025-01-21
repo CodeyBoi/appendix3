e14063dd359605ba5c5487e2da3d6e495c714da1
CodeyBoi <hannes.ryberg00@gmail.com>
2025-01-21 21:16:52 +0100

    Updated strecklist download name

--- src/app/admin/streck/download.tsx
+++ src/app/admin/streck/download.tsx
@@ -262,7 +262,9 @@ const generateStreckList = (activeCorps: ActiveCorps[], items: Item[]) => {
     sheet.getCell(rowIndex, lastCol).border = bottomRight;
   }
 
-  const filename = `Tom strecklista ${dayjs().format('YYYY-MM-DD HH:mm')}.xlsx`;
+  const filename = `Tom_strecklista_${dayjs().format(
+    'YYYY-MM-DD_HHmmss',
+  )}.xlsx`;
   downloadXLSX(workbook, filename);
 };
 
