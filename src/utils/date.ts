f4bcc030ed3bd50c1687d0b1cba82f62f7f055f8
CodeyBoi <hannes.ryberg00@gmail.com>
2025-01-30 12:59:13 +0100

    Added date util function for calculating gig calender datetimes

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 export const newUTCDate = (year: number, month: number, day: number) =>
   new Date(Date.UTC(year, month, day, 0, 0, 0, 0));
 
@@ -61,3 +63,59 @@ export const startOperatingYear = 2010;
 export const isChristmas = () => new Date().getMonth() === 11;
 
 export const isJuly = () => new Date().getMonth() === 6;
+
+const parseGigTime = (date: Date, time: string) => {
+  // `time` is a string describing a time of day in UTC+1
+  // Only consider the first two characters (incase `time` is '21:15ish')
+  const [hour = NaN, minute = NaN] = time
+    .split(/[:.]/)
+    .map((s) => parseInt(s.slice(0, 2)));
+  if (isNaN(hour) || isNaN(minute)) {
+    return undefined;
+  }
+  const dateTime = new Date(date);
+  dateTime.setUTCHours(hour - 1, minute);
+  return dateTime;
+};
+
+export const getGigCalenderDates = (gig: {
+  date: Date;
+  meetup: string;
+  start: string;
+}) => {
+  const meetup = parseGigTime(gig.date, gig.meetup);
+  const start = parseGigTime(gig.date, gig.start);
+
+  if (!meetup && !start) {
+    return undefined;
+  } else if (meetup && !start) {
+    return {
+      start: meetup,
+      end: dayjs(meetup).add(2, 'hours').toDate(),
+    };
+  } else if (!meetup && start) {
+    return {
+      start,
+      end: dayjs(start).add(2, 'hours').toDate(),
+    };
+  }
+
+  if (!meetup || !start) {
+    throw Error(
+      'This should be unreachable as the above would filter out this case',
+    );
+  }
+
+  // Handle the edge case where e.g `gig.meetup === '23:30' && gig.start === '00:30'`
+  // (i.e. `start` is before `meetup`)
+  if (start.getTime() < meetup.getTime()) {
+    start.setDate(start.getDate() + 1);
+  }
+
+  const delta = start.getTime() - meetup.getTime();
+  const end = new Date(meetup.getTime() + 2 * delta + 30 * 60 * 1000);
+  return {
+    start: meetup,
+    end,
+  };
+};
