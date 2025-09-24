401f46ce8de278e69f78b8e083ee85979f4b1840
CodeyBoi <hannes.ryberg00@gmail.com>
2025-09-24 16:36:05 +0200

    Update section leader permission role when updating section leaders

--- src/server/trpc/router/section.ts
+++ src/server/trpc/router/section.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { router, restrictedProcedure } from '../trpc';
+import { filterNone } from 'utils/array';
 
 export const sectionRouter = router({
   getSectionLeader: restrictedProcedure('manageSections')
@@ -29,6 +30,7 @@ export const sectionRouter = router({
     .input(z.object({ sectionId: z.number(), corpsId: z.string() }))
     .mutation(async ({ ctx, input }) => {
       const { sectionId, corpsId } = input;
+
       const section = await ctx.prisma.section.update({
         where: { id: sectionId },
         data: {
@@ -39,6 +41,73 @@ export const sectionRouter = router({
           },
         },
       });
+
+      // Sync section leader permission roles
+      const sectionLeaderRole = await ctx.prisma.role.findUnique({
+        where: {
+          name: 'Stämledare',
+        },
+      });
+      if (sectionLeaderRole) {
+        // Remove "Stämledare" role from all corps
+        const oldSectionLeaders = await ctx.prisma.corps.findMany({
+          select: {
+            id: true,
+          },
+          where: {
+            roles: {
+              some: {
+                id: sectionLeaderRole.id,
+              },
+            },
+          },
+        });
+        await Promise.all(
+          oldSectionLeaders.map((leader) =>
+            ctx.prisma.corps.update({
+              where: {
+                id: leader.id,
+              },
+              data: {
+                roles: {
+                  disconnect: {
+                    id: sectionLeaderRole.id,
+                  },
+                },
+              },
+            }),
+          ),
+        );
+
+        // Readd all "Stämledare" roles
+        const sections = await ctx.prisma.section.findMany({
+          select: {
+            leader: {
+              select: {
+                id: true,
+              },
+            },
+          },
+        });
+        const leaders = filterNone(sections.map((section) => section.leader));
+        await Promise.all(
+          leaders.map((leader) =>
+            ctx.prisma.corps.update({
+              where: {
+                id: leader.id,
+              },
+              data: {
+                roles: {
+                  connect: {
+                    id: sectionLeaderRole.id,
+                  },
+                },
+              },
+            }),
+          ),
+        );
+      }
+
       return section;
     }),
 });
