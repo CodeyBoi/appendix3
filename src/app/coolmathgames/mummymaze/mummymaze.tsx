124f4969b2253039335192a26428088fe09c6a2b
CodeyBoi <hannes.ryberg00@gmail.com>
2025-10-13 00:44:26 +0200

    Add skeleton for Mummy Maze

---
+++ src/app/coolmathgames/mummymaze/mummymaze.tsx
@@ -0,0 +1,9 @@
+const MummyMaze = () => {
+  return (
+    <div>
+      <h2>BIIG MUMMY MAZE</h2>
+    </div>
+  );
+};
+
+export default MummyMaze;
---
+++ src/app/coolmathgames/mummymaze/page.tsx
@@ -0,0 +1,11 @@
+import MummyMaze from './mummymaze';
+
+const MummyMazePage = () => {
+  return (
+    <div>
+      <MummyMaze />
+    </div>
+  );
+};
+
+export default MummyMazePage;
