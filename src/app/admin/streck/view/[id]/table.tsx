8c957a370de51d4e68e018dec6046a8ab12b99a6
CodeyBoi <hannes.ryberg00@gmail.com>
2026-01-09 10:58:01 +0100

    Streck hotfix

--- src/app/admin/streck/view/[id]/table.tsx
+++ src/app/admin/streck/view/[id]/table.tsx
@@ -60,46 +60,49 @@ const StreckList = async ({ id }: StreckListProps) => {
       </table>
     );
   } else {
-    <table className='table-auto'>
-      <thead>
-        <tr className='divide-x divide-solid border-b text-left align-bottom text-xs dark:divide-neutral-800'>
-          <th className='px-1 text-center'>#</th>
-          <th className='px-1'>{lang('Förnamn', 'First name')}</th>
-          <th className='px-1'>{lang('Efternamn', 'Surname')}</th>
-          <th className='px-1'>{lang('Kostnad', 'Cost')}</th>
-          {listType === 'deposit' && (
-            <th className='px-1'>
-              {lang('Verifikatsnr.', 'Verification nr.')}
-            </th>
-          )}
-          <th className='px-1'>{lang('Anteckning', 'Note')}</th>
-        </tr>
-      </thead>
+    return (
+      <table className='table-auto'>
+        <thead>
+          <tr className='divide-x divide-solid border-b text-left align-bottom text-xs dark:divide-neutral-800'>
+            <th className='px-1 text-center'>#</th>
+            <th className='px-1'>{lang('Förnamn', 'First name')}</th>
+            <th className='px-1'>{lang('Efternamn', 'Surname')}</th>
+            <th className='px-1'>{lang('Kostnad', 'Cost')}</th>
+            {listType === 'deposit' && (
+              <th className='px-1'>
+                {lang('Verifikatsnr.', 'Verification nr.')}
+              </th>
+            )}
+            <th className='px-1'>{lang('Anteckning', 'Note')}</th>
+          </tr>
+        </thead>
 
-      <tbody className='divide-y divide-solid text-sm dark:divide-neutral-800'>
-        {streckList.transactions
-          .sort(
-            (a, b) =>
-              a.verificationNumber?.localeCompare(b.verificationNumber ?? '') ??
-              -1,
-          )
-          .map(({ corps, pricePer, verificationNumber, note }) => (
-            <tr className='divide-x divide-solid dark:divide-neutral-800'>
-              <td className='px-1 text-right'>
-                {corps.number?.toString() ??
-                  (corps.bNumber ? 'b' + corps.bNumber.toString() : '')}
-              </td>
-              <td className='px-1'>{corps.firstName.trim()}</td>
-              <td className='px-1'>{corps.lastName.trim()}</td>
-              <td className='px-1'>{Math.abs(pricePer)}</td>
-              {listType === 'deposit' && verificationNumber && (
-                <td className='px-1'>{verificationNumber}</td>
-              )}
-              <td className='px-1'>{note}</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>;
+        <tbody className='divide-y divide-solid text-sm dark:divide-neutral-800'>
+          {streckList.transactions
+            .sort(
+              (a, b) =>
+                a.verificationNumber?.localeCompare(
+                  b.verificationNumber ?? '',
+                ) ?? -1,
+            )
+            .map(({ corps, pricePer, verificationNumber, note }) => (
+              <tr className='divide-x divide-solid dark:divide-neutral-800'>
+                <td className='px-1 text-right'>
+                  {corps.number?.toString() ??
+                    (corps.bNumber ? 'b' + corps.bNumber.toString() : '')}
+                </td>
+                <td className='px-1'>{corps.firstName.trim()}</td>
+                <td className='px-1'>{corps.lastName.trim()}</td>
+                <td className='px-1'>{Math.abs(pricePer)}</td>
+                {listType === 'deposit' && verificationNumber && (
+                  <td className='px-1'>{verificationNumber}</td>
+                )}
+                <td className='px-1'>{note}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    );
   }
 };
 
