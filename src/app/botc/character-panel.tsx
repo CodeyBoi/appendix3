f71c9673bee3c91ded2d0dc2442948bba471b48c
CodeyBoi <hannes.ryberg00@gmail.com>
2026-07-09 23:12:00 +0200

    Character sheet wiki links

--- src/app/botc/character-panel.tsx
+++ src/app/botc/character-panel.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 import { cn } from 'utils/class-names';
 
 interface BotcCharacterPanelProps {
   name: string;
   imgSrc: string;
+  imgLink?: string;
   description: string;
   showDescription: boolean;
 }
@@ -10,20 +12,30 @@ interface BotcCharacterPanelProps {
 const BotcCharacterPanel = ({
   name,
   imgSrc,
+  imgLink,
   description,
   showDescription,
 }: BotcCharacterPanelProps) => {
+  const imgElement = (
+    <img
+      className={cn(
+        'h-8 w-8 scale-150 lg:h-12 lg:w-12',
+        imgSrc.includes('Fall_of_Rome') && 'translate-y-1.5',
+      )}
+      loading='lazy'
+      src={imgSrc}
+    />
+  );
   return (
     <div className='flex flex-col gap-2'>
       <div className='flex items-center gap-4'>
-        <img
-          className={cn(
-            'h-8 w-8 scale-150 lg:h-12 lg:w-12',
-            imgSrc.includes('Fall_of_Rome') && 'translate-y-1.5',
-          )}
-          loading='lazy'
-          src={imgSrc}
-        />
+        {imgLink ? (
+          <Link target='_blank' href={imgLink}>
+            {imgElement}
+          </Link>
+        ) : (
+          imgElement
+        )}
         <h5 className='hidden lg:block'>{name}</h5>
         <h6 className='lg:hidden'>{name}</h6>
       </div>
