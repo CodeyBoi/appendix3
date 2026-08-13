be0d4399e65b0843d4518d6805fe3257c865d962
CodeyBoi <hannes.ryberg00@gmail.com>
2026-08-13 15:19:54 +0200

    Pass corps language correctly

--- src/app/(authenticated)/admin/streck/accounts/list.tsx
+++ src/app/(authenticated)/admin/streck/accounts/list.tsx
@@ -25,16 +25,13 @@ interface StreckAccountsListProps {
   balances: StreckAccount[];
 }
 
-const generateStreckMailtoLink = (
-  account: StreckAccount,
-  languageArg: string = 'sv',
-) => {
+const generateStreckMailtoLink = (account: StreckAccount) => {
   const bodyTemplates: Record<Language, string> = {
     sv: `Hej <NAME>!\n\nDu ligger nu minus på ditt strecksaldo med <NEGATIVE-BALANCE> kr och du behöver därför fylla på ditt streckkonto. Du kan göra det via bankgiro 669-8567 eller Swish 123-388 68 76. Vid Swish tillkommer avgift på 2kr som dras av från insättningen. Gör du en internationell överföring så är IBAN SE51 6000 0000 0003 6666 1272, BIC är HANDSESS.\n\nFör att Bleckhornen ska kunna drivas på ett sätt som är ekonomiskt hållbart måste medlemmar och provelever alltid sträva efter att ha ett neutralt eller positivt strecksaldo. Streckkontot är inte ett kreditkonto, utan man fyller på först och därefter streckar man. Du får inte fortsätta strecka innan du fyllt på ditt streckkonto, gör du det ändå kommer det debiteras extra. Styrelsen kan också neka dig att gå på corpsaftnar eller andra evenemang om du inte fyller på streckkontot.\n\nKul-att-strecka-fjång!`,
     en: `Hi <NAME>!\n\nYou are currently negative on your streckkonto balance by <NEGATIVE-BALANCE> SEK, and therefore need to top up. You can do this via bankgiro 669-8567 or Swish 123-388 68 76. For Swish payments, a fee of 2 SEK is deducted from the deposit. For international payments the IBAN is SE51 6000 0000 0003 6666 1272 and the BIC is HANDSESS.\n\nFor Bleckhornen to be run in a financially sustainable way, members and provelever must always strive to have a neutral or positive streckkonto balance. The streckkonto is not a credit account; you top it up first and then you may strecka. You may not continue to strecka before you have refilled your account, if you do so anyways you will be charged extra. The board may also deny you entry to corps evenings or other events if you do not top up your streckkonto.\n\nFun-to-strecka-fjång!`,
   };
   const language =
-    languageArg in bodyTemplates ? (languageArg as Language) : 'sv';
+    account.language in bodyTemplates ? (account.language as Language) : 'sv';
   const subject =
     language === 'sv'
       ? 'Ditt strecksaldo är negativt'
