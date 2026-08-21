7fa971415c107d1d52521cb7ca84002127d314e4
CodeyBoi <hannes.ryberg00@gmail.com>
2026-08-21 13:00:35 +0200

    Fix buggy header navbar

--- src/app/navbar-button.tsx
+++ src/app/navbar-button.tsx
@@ -2,36 +2,28 @@
 
 import Burger from 'components/burger';
 import { usePathname } from 'next/navigation';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { cn } from 'utils/class-names';
 import { isJuly, isPrideMonth } from 'utils/date';
 
 interface NavbarButtonProps {
+  open: boolean;
+  setOpen: (isOpen: boolean) => void;
   children: ReactNode;
   currentDate?: Date;
 }
 
 const NavbarButton = ({
+  open,
+  setOpen,
   children,
   currentDate = new Date(),
 }: NavbarButtonProps) => {
-  const [open, setOpen] = useState(false);
-  const [isClosing, setIsClosing] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
     setOpen(false);
   }, [pathname]);
-
-  useEffect(() => {
-    setIsClosing(true);
-    const timeout = setTimeout(() => {
-      setIsClosing(false);
-    }, 200);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [open]);
 
   return (
     <>
@@ -50,9 +42,8 @@ const NavbarButton = ({
       />
       <div
         className={cn(
-          'absolute inset-0 z-20 mt-14 h-[100vh-56px] w-screen transition-colors duration-200',
+          'fixed inset-0 z-20 mt-14 h-screen w-screen transition-colors duration-200',
           open ? 'bg-black/50' : 'pointer-events-none bg-transparent',
-          (open || isClosing) && 'h-screen',
         )}
         onClick={() => {
           setOpen(false);
@@ -60,9 +51,8 @@ const NavbarButton = ({
       />
       <div
         className={cn(
-          'absolute right-0 top-0 z-30 mt-14 rounded-md shadow-lg transition-transform duration-200',
+          'fixed right-0 top-0 z-30 mt-14 rounded-md shadow-lg transition-transform duration-200',
           !open && 'translate-x-72',
-          !open && !isClosing && 'fixed',
         )}
       >
         <div
