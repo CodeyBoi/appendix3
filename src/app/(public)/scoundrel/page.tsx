8ad1510c50a5d5e4255bf9614f723fcf79c0b809
Hannes Ryberg <47188830+CodeyBoi@users.noreply.github.com>
2026-08-22 14:10:03 +0200

    Scoundrel: Make highscore client component (#465)

--- src/app/(public)/scoundrel/page.tsx
+++ src/app/(public)/scoundrel/page.tsx
@@ -1,16 +1,11 @@
 import ScoundrelElement from './scoundrel-element';
-import { Suspense } from 'react';
-import Loading from 'components/loading';
-import { lang } from 'utils/language';
 import ScoundrelHighscore from './highscore';
 
 const ScoundrelPage = () => {
   return (
     <div className='flex flex-col gap-2'>
       <ScoundrelElement />
-      <Suspense fallback={<Loading msg={lang('Hämtar...', 'Fetching...')} />}>
-        <ScoundrelHighscore />
-      </Suspense>
+      <ScoundrelHighscore />
     </div>
   );
 };
